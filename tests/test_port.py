fd60e73c3da3703740d9cd31e145de8f3ecec966
Akshay Venkatesh <akvenkatesh@nvidia.com>
2019-04-24 17:46:22 -0700

    test cleanup

--- tests/test_port.py
+++ tests/test_port.py
@@ -5,15 +5,13 @@ import ucp
 async def talk_to_client(ep, listener):
     recv_req = await ep.recv_future()
     recv_msg = ucp.get_obj_from_msg(recv_req)
-    #print('Client connected')
+    ucp.destroy_ep(ep)
     ucp.stop_listener(listener)
-    #ucp.destroy_ep(ep)
     
 async def talk_to_server(ip, port):
     ep = ucp.get_endpoint(ip, port)
     await ep.send_obj(bytes(b"42"))
-    #print('Client done')
-    #ucp.destroy_ep(ep)
+    ucp.destroy_ep(ep)
 
 @pytest.mark.asyncio
 async def test_zero_port():
@@ -22,8 +20,6 @@ async def test_zero_port():
     assert 0 < listener.port < 2**16
     
     ip = ucp.get_address()
-    #print(ip)
-    #print(listener.port)
     await asyncio.gather(
         listener.coroutine,
         talk_to_server(ip.encode(), listener.port)
