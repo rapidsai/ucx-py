8d08303fb4842c85772297e46434ce31551585c6
Matthew Rocklin <mrocklin@gmail.com>
2019-06-03 13:25:28 -0700

    Add Endpoint.__str__ implementation (#115)
    This should help when debugging

--- tests/test_addr.py
+++ tests/test_addr.py
@@ -12,7 +12,7 @@ async def talk_to_client(ep, listener):
     assert(ep.remote_port() == None)
     ucp.destroy_ep(ep)
     ucp.stop_listener(listener)
-    
+
 async def talk_to_server(ip, port):
     ep = await ucp.get_endpoint(ip, port)
     await ep.send_obj(bytes(ip))
@@ -20,6 +20,10 @@ async def talk_to_server(ip, port):
     assert(ep.remote_ip() == ip.decode())
     assert(ep.local_ip() == ip.decode())
     assert(ep.local_port() == None)
+
+    assert str(ep.local_address) in str(ep)
+    assert str(ep.remote_address) in str(ep)
+
     ucp.destroy_ep(ep)
 
 @pytest.mark.asyncio
@@ -27,7 +31,7 @@ async def test_zero_port():
     ucp.init()
     listener = ucp.start_listener(talk_to_client, is_coroutine=True)
     assert 0 < listener.port < 2**16
-    
+
     ip = ucp.get_address()
     await asyncio.gather(
         listener.coroutine,
