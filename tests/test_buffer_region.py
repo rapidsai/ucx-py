6d46eb415bdd98fe3d9aad82815d1b60451a3507
Benjamin Zaitlen <quasiben@gmail.com>
2019-07-16 07:17:15 -0700

    add buffer region test for cudf objects

--- tests/test_buffer_region.py
+++ tests/test_buffer_region.py
@@ -1,4 +1,5 @@
 import pytest
+import functools
 
 import ucp
 
@@ -47,3 +48,37 @@ def test_cupy(dtype):
 
     result = cupy.asarray(buffer_region)
     cupy.testing.assert_array_equal(result, arr)
+
+
+@pytest.mark.parametrize(
+    "g",
+    [
+        lambda cudf: cudf.Series([1, 2, 3]),
+        pytest.param(lambda cudf: cudf.Series([1, 2, 3], index=[4, 5, 6]),
+            marks=pytest.mark.xfail),
+        lambda cudf: cudf.Series([1, None, 3]),
+        lambda cudf: cudf.Dataframe({'A': [1, 2, 3]}),
+    ]
+)
+def test_cudf(g):
+    cudf = pytest.importorskip('cudf')
+    deserialize = pytest.importorskip("distributed.protocol").deserialize
+    serialize = pytest.importorskip("distributed.protocol").serialize
+    from dask.dataframe.utils import assert_eq
+
+    cuda_serialize = functools.partial(serialize, serializers=["cuda"])
+    cuda_deserialize = functools.partial(deserialize, deserializers=["cuda"])
+
+    cdf = g(cudf)
+    header, frames = cuda_serialize(cdf)
+
+    gpu_buffers = []
+    for f in frames:
+        if hasattr(f, '__cuda_array_interface__'):
+           buffer_region = ucp.BufferRegion()
+           buffer_region.populate_cuda_ptr(f)
+           gpu_buffers.append(buffer_region)
+        else:
+            gpu_buffers.append(f)
+    res = cuda_deserialize(header, gpu_buffers)
+    assert_eq(res, cdf)
