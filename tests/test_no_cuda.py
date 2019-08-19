0e89ef137e27e06a3334229211bdbac1b3da76d9
Benjamin Zaitlen <quasiben@gmail.com>
2019-08-19 10:03:44 -0700

    ammend skip message and lint

--- tests/test_no_cuda.py
+++ tests/test_no_cuda.py
@@ -2,8 +2,8 @@ import pytest
 
 import ucp
 
-pytestmark = pytest.mark.skipif(ucp.HAS_CUDA, reason="has CUDA")
-msg = 'not compiled with CUDA'
+msg = "Tests are only for UCP compiled without CUDA"
+pytestmark = pytest.mark.skipif(ucp.HAS_CUDA, reason=msg)
 
 
 def test_alloc_cuda_raises():
@@ -21,6 +21,7 @@ def test_free_cuda_raises():
 def test_set_cuda_dev_raises():
     with pytest.raises(ValueError, match=msg):
         ucp.set_cuda_dev(0)
+
 
 # No test for Message.check_mem, as I think it's impossible
 # to construct a Message with a CUDA BufferRegion without
