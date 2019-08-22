6ede1ef437ba94d74acb7fa8e7f67b34f15ba98b
Mads R. B. Kristensen <madsbk@gmail.com>
2019-08-22 05:30:20 -0700

    Removed `struct data_buf`, now BufferRegon has the pointer directly

--- ucp/_libs/src/buffer_ops.h
+++ ucp/_libs/src/buffer_ops.h
@@ -8,17 +8,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-struct data_buf *populate_buffer_region(void *src);
-struct data_buf *populate_buffer_region_with_ptr(unsigned long long int);
-void *return_ptr_from_buf(struct data_buf *db);
-struct data_buf *allocate_host_buffer(ssize_t length);
-int set_host_buffer(struct data_buf *db, int c, ssize_t length);
-int check_host_buffer(struct data_buf *db, int c, ssize_t length);
-int free_host_buffer(struct data_buf *buf);
-
-/* cuda */
+void* malloc_host(size_t length);
+void* malloc_cuda(size_t length);
+void free_host(void* mem_ptr);
+void free_cuda(void* mem);
 int set_device(int device);
-int set_cuda_buffer(struct data_buf *db, int c, ssize_t length);
-int check_cuda_buffer(struct data_buf *db, int c, ssize_t length);
-int free_cuda_buffer(struct data_buf *buf);
-struct data_buf *allocate_cuda_buffer(ssize_t length);
--- ucp/_libs/src/common.h
+++ ucp/_libs/src/common.h
@@ -1,10 +1,6 @@
 #ifndef UCX_PY_COMMON_H
 #define UCX_PY_COMMON_H
 
-struct data_buf {
-    void            *buf;
-};
-
 #define DEBUG 0
 
 #if DEBUG
