396056c80736f599f65dfcf82f97ecfce0c439bc
Ashwin Srinath <shwina@users.noreply.github.com>
2019-09-05 07:39:59 -0700

    Move testing utilities to utils.py. Further cleanup/improvements in dask/cudf/ucx tests

--- tests/test-ucx-w-dask-workers.py
+++ tests/test-ucx-w-dask-workers.py
@@ -1,199 +1,146 @@
+import asyncio
 import os
 import pytest
-from distributed.utils import format_bytes
+
 import numpy as np
-import asyncio
 import dask.dataframe as dd
-from distributed import Scheduler, Worker, Client, Nanny, wait
-from distributed.utils import log_errors
 
 from cudf.tests.utils import assert_eq
 
-#async with Nanny(s.address, protocol='ucx', nthreads=1,
-# nanny is really a worker running on a defined CUDA DEVICE
-protocol = 'ucx'
-interface = 'ib0'  # Ff changing CUDA_VISIBLE_DEVICES CHECK IB Controller
+from .utils import dgx_ucx_cluster, cudf_obj_generators
 
-w_env = {
-    "UCX_RNDV_SCHEME": "put_zcopy",
-    "UCX_MEMTYPE_CACHE": "n",
-    "UCX_TLS": "rc,cuda_copy,cuda_ipc",
-    "CUDA_VISIBLE_DEVICES": "0,1",
-}
-os.environ.update(w_env)
-
-w_env_2 = w_env.copy()
-w_env_2["CUDA_VISIBLE_DEVICES"] = "1,0"
-
-async def f(cudf_obj_generator):
-    async with Scheduler(protocol=protocol, interface=interface,
-    dashboard_address=':8789') as s:
-        async with Nanny(s.address, protocol=protocol, nthreads=1,
-                memory_limit='32GB', interface=interface,
-                env=w_env,
-                ) as w:
-            async with Nanny(s.address, protocol=protocol,memory_limit='32gb',
-                    env=w_env_2, interface=interface,
-                    nthreads=1) as w2:
-                async with Client(s.address, asynchronous=True) as c:
-                    with log_errors():
-
-                        def set_nb_context(x=None):
-                            import numba.cuda
-                            try:
-                                numba.cuda.current_context()
-                            except Exception:
-                                print("FAILED EXCEPTION!")
-
-                        print(f"SETTING CUDA CONTEXT ON WORKERS: {w.worker_address} / {w2.worker_address}")
-                        out = await c.run(set_nb_context, workers=[w.worker_address, w2.worker_address])
-                        print(set_nb_context())
-
-                        print("Creating and Mapping CUDA Objects")
-                        # offset worker two for unique hash names inside of dask
-                        N = 100
-                        left = c.map(cudf_obj_generator,
-                                     range(N), workers=[w.worker_address])
-                        right = c.map(cudf_obj_generator,
-                                        range(1, N+1), workers=[w2.worker_address])
-                        print("Gather CUDA Objects")
-                        futures = c.map(lambda x, y: (x,y), left, right, priority=10)
-                        results = await c.gather(futures, asynchronous=True)
-
-                        print("ALL DONE!")
-                        
-async def g():
-    async with Scheduler(protocol=protocol, interface=interface,
-    dashboard_address=':8789') as s:
-        async with Nanny(s.address, protocol=protocol, nthreads=1,
-                memory_limit='32GB', interface=interface,
-                env=w_env,
-                ) as w1:
-            async with Nanny(s.address, protocol=protocol,memory_limit='32gb',
-                    env=w_env_2, interface=interface,
-                    nthreads=1) as w2:
-                async with Client(s.address, asynchronous=True) as c:
-                    with log_errors():
-                        import dask.array as da
-                        import dask_cudf
-                        import numpy as np
-                        import cudf
-
-                        n_rows = 500_000_000
-                        chunks = n_rows // 10
-
-                        res_x = dask_cudf.from_cudf(cudf.Series(np.random.rand(n_rows)), npartitions=n_rows // chunks)
-                        res_y = dask_cudf.from_cudf(cudf.Series(np.random.rand(n_rows)), npartitions=n_rows // chunks)
-
-                        res_x = res_x.persist(workers=[w1.worker_address])
-                        res_y = res_y.persist(workers=[w2.worker_address])
-                        
-                        res = (res_x + res_y).persist()
-                        out = await c.compute(res.head(compute=False))
-                        print(out)
-
-async def h(left_n_rows):
-    async with Scheduler(protocol=protocol, interface=interface,
-    dashboard_address=':8789') as s:
-        async with Nanny(s.address, protocol=protocol, nthreads=1,
-                memory_limit='32GB', interface=interface,
-                env=w_env,
-                ) as w1:
-            async with Nanny(s.address, protocol=protocol,memory_limit='32gb',
-                    env=w_env_2, interface=interface,
-                    nthreads=1) as w2:
-                async with Client(s.address, asynchronous=True) as c:
-                    with log_errors():
-                        import dask.array as da
-                        import dask_cudf
-                        import numpy as np
-                        import cupy
-                        import cudf
-
-                        def set_nb_context(x=None):
-                            import numba.cuda
-                            try:
-                                numba.cuda.current_context()
-                            except Exception:
-                                print("FAILED EXCEPTION!")
-                                
-                        out = await c.run(
-                            set_nb_context,
-                            workers=[w1.worker_address, w2.worker_address]
-                        )
-                        
-                        # left = dd.concat([
-                        #     da.random.random(left_n_rows).to_dask_dataframe(columns='x'),
-                        #     da.random.randint(0, 10, size=left_n_rows).to_dask_dataframe(columns='id'),
-                        # ], axis=1)
-
-                        # left = await left.map_partitions(cudf.from_pandas).persist(workers=[w1.worker_address])
-                        # print(f"Left npartitions: {left.npartitions}")
-
-                        # out = await left.repartition(npartitions=10).persist()
-
-
-                        future = c.submit(dataframe, left_n_rows)
-
-                        npartitions = 10
-                        results = []
-                        for i in range(npartitions):
-                            start = i * (left_n_rows // npartitions)
-                            end = (i + 1) * (left_n_rows // npartitions)
-                            worker = (w1.worker_address, w2.worker_address)[i % 2]
-                            results.append(c.submit(lambda x: x.iloc[start:end], future, workers=[worker]))
-
-                        del future
-                        for result in results:
-                            await result
-                        
-def column(x):
-    import cudf
-    import numpy as np
-    return cudf.Series(np.arange(100_000))._column
-
-
-def series(x):
-    import cudf
-    import numpy as np
-    return cudf.Series(np.arange(100_000))
-
-
-def dataframe(size):
-    import cudf
-    import numpy as np
-    
-    return cudf.DataFrame(
-        {"a": np.random.random(size), "b": np.random.random(size)},
-        index=np.random.randint(size, size=size),
-    )
-
-def cupy(x):
-    import numpy as np
-    import cupy as cp
-
-    return cp.asarray(np.arange(100_000_000))
 
 mark_is_not_dgx = not os.path.isfile("/etc/dgx-release")
 
+
 @pytest.mark.skipif(mark_is_not_dgx, reason="Not a DGX")
 @pytest.mark.asyncio
-@pytest.mark.parametrize("cudf_obj_generator", [
-    column,
-    series,
-    cupy,
-    dataframe
+@pytest.mark.parametrize("cudf_obj", [
+    "column",
+    "series",
+    "cupy",
+    "dataframe"
 ])
-async def test_send_recv_cuda(event_loop, cudf_obj_generator):
-    await f(cudf_obj_generator)
+@pytest.mark.parametrize("size", [
+    100,
+    1_000,
+    10_000,
+    100_000,
+    500_000,
+    1_000_000,
+    10_000_000,
+    100_000_000,
+    500_000_000
+])
+@pytest.mark.parametrize("num_objects", [
+    1,
+    2,
+    100,
+    1000,
+    10_000,
+    100_000,
+    1_000_000
+])
+async def test_send_recv_objects(event_loop, cudf_obj, size, num_objects):
+    async with dgx_ucx_cluster() as (s, w1, w2, c):
+        cudf_obj_generator = cudf_obj_generators[cudf_obj]
+        # offset worker two for unique hash names inside of dask
+        left = c.map(lambda x  : cudf_obj_generator(size),
+                     range(num_objects), workers=[w1.worker_address])
+        right = c.map(lambda x : cudf_obj_generator(size),
+                        range(1, num_objects+1), workers=[w2.worker_address])
+        futures = c.map(lambda x, y: (x,y), left, right, priority=10)
+        results = await c.gather(futures, asynchronous=True)
 
 @pytest.mark.asyncio
-async def test_dask_array_map_partitions():
-    await g()
+@pytest.mark.skipif(mark_is_not_dgx, reason="Not a DGX")
+@pytest.mark.parametrize("size", [
+    100,
+    1_000,
+    10_000,
+    100_000,
+    500_000,
+    1_000_000,
+    10_000_000,
+    100_000_000,
+    500_000_000
+])
+async def test_dask_array_sum(event_loop, size):
+    async with dgx_ucx_cluster() as (s, w1, w2, c):
+        import dask.array as da
+        import dask_cudf
+        import numpy as np
+        import cudf
 
-@pytest.mark.parametrize(
-    "left_n_rows",
-    [100, 10_000, 100_000, 500_000, 1_000_000, 10_000_000, 100_000_000, 500_000_000])
+        size = 500_000_000
+        chunks = size // 10
+
+        res_x = dask_cudf.from_cudf(cudf.Series(np.random.rand(size)), npartitions=size // chunks)
+        res_y = dask_cudf.from_cudf(cudf.Series(np.random.rand(size)), npartitions=size // chunks)
+
+        res_x = res_x.persist(workers=[w1.worker_address])
+        res_y = res_y.persist(workers=[w2.worker_address])
+
+        res = (res_x + res_y).persist()
+        out = await c.compute(res.head(compute=False))
+
+
+@pytest.mark.skipif(mark_is_not_dgx, reason="Not a DGX")
 @pytest.mark.asyncio
-async def test_dask_array_join(left_n_rows):
-    await h(left_n_rows)
+@pytest.mark.parametrize("size", [
+    100,
+    1_000,
+    10_000,
+    100_000,
+    500_000,
+    1_000_000,
+    10_000_000,
+    100_000_000,
+    500_000_000
+])
+@pytest.mark.parametrize("npartitions", [
+    1,
+    2,
+    10,
+    100
+])
+async def test_dask_array_repartition(event_loop, size, npartitions):
+    async with dgx_ucx_cluster() as (s, w1, w2, c):
+        df = dd.from_pandas(dataframe(size))
+        df = await df.map_partitions(cudf.from_pandas).persist(workers=[w1.worker_address])
+        out = await df.repartition(npartitions).persist()
+
+
+@pytest.mark.skipif(mark_is_not_dgx, reason="Not a DGX")
+@pytest.mark.asyncio
+@pytest.mark.parametrize("size", [
+    100,
+    1_000,
+    10_000,
+    100_000,
+    500_000,
+    1_000_000,
+    10_000_000,
+    100_000_000,
+    500_000_000
+])
+@pytest.mark.parametrize("npartitions", [
+    1,
+    2,
+    10,
+    100
+])
+async def test_futures_repartition(event_loop, size, npartitions):
+    async with dgx_ucx_cluster() as (s, w1, w2, c):
+        future = c.submit(dataframe, left_n_rows)
+
+        results = []
+        for i in range(npartitions):
+            start = i * (left_n_rows // npartitions)
+            end = (i + 1) * (left_n_rows // npartitions)
+            worker = (w1.worker_address, w2.worker_address)[i % 2]
+            results.append(c.submit(lambda x: x.iloc[start:end], future, workers=[worker]))
+
+        del future
+        for result in results:
+            await result
--- tests/utils.py
+++ tests/utils.py
@@ -1,7 +1,13 @@
 import os
-import ucp
 import contextlib
+
 import pytest
+import numpy as np
+from distributed import Scheduler, Worker, Client, Nanny, wait
+from distributed.utils import log_errors
+
+import cudf
+import ucp
 
 normal_env = {
     "UCX_RNDV_SCHEME": "put_zcopy",
@@ -24,3 +30,91 @@ def ucp_init():
         yield
     finally:
         ucp.fin()
+
+
+@contextlib.asynccontextmanager
+async def dgx_ucx_cluster(
+        protocol='ucx',
+        interface='ib0',
+        cuda_visible_devices=(0,1),
+):
+    d1, d2 = cuda_visible_devices
+    worker_1_env = {
+        "UCX_RNDV_SCHEME": "put_zcopy",
+        "UCX_MEMTYPE_CACHE": "n",
+        "UCX_TLS": "rc,cuda_copy,cuda_ipc",
+        "CUDA_VISIBLE_DEVICES": f"{d1},{d2}"
+    }
+    os.environ.update(worker_1_env)
+    worker_2_env = worker_1_env.copy()
+    worker_2_env["CUDA_VISIBLE_DEVICES"] = f"{d2},{d1}"
+
+    async with Scheduler(
+            protocol=protocol,
+            interface=interface,
+            dashboard_address=':8789'
+    ) as s:
+        async with Nanny(
+                s.address,
+                protocol=protocol,
+                nthreads=1,
+                memory_limit='32GB',
+                interface=interface,
+                env=worker_1_env,
+        ) as w1:
+            async with Nanny(
+                    s.address,
+                    protocol=protocol,
+                    nthreads=1,
+                    memory_limit='32gb',
+                    interface=interface,
+                    env=worker_2_env,
+            ) as w2:
+                async with Client(s.address, asynchronous=True) as c:
+                    with log_errors():
+                        def set_nb_context(x=None):
+                            import numba.cuda
+                            try:
+                                numba.cuda.current_context()
+                            except Exception:
+                                print("Could not set context!")
+                        out = await c.run(
+                            set_nb_context,
+                            workers=[w1.worker_address, w2.worker_address]
+                        )
+                        yield s, w1, w2, c
+                        
+def make_column(size):
+    import cudf
+    import numpy as np
+    return cudf.Series(np.arange(size))._column
+
+
+def make_series(size):
+    import cudf
+    import numpy as np
+    return cudf.Series(np.arange(size))
+
+
+def make_dataframe(size):
+    import cudf
+    import numpy as np
+    
+    return cudf.DataFrame(
+        {"a": np.random.random(size), "b": np.random.random(size)},
+        index=np.random.randint(size, size=size),
+    )
+
+def make_cupy(size):
+    import numpy as np
+    import cupy as cp
+
+    return cp.asarray(np.arange(size))
+
+cudf_obj_generators = {
+    "column": make_column,
+    "series": make_series,
+    "cupy": make_cupy,
+    "dataframe": make_dataframe
+}
+        
