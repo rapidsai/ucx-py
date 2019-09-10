8b6d9099b84bed5da0f6dc98e4fc570a17763d31
Mads R. B. Kristensen <madsbk@gmail.com>
2019-09-10 04:57:25 -0700

    test: added loop exception handler

--- tests/test_tiny.py
+++ tests/test_tiny.py
@@ -1,6 +1,7 @@
 import pickle
 import asyncio
 import pytest
+import sys
 from contextlib import asynccontextmanager
 
 import ucp
@@ -17,7 +18,7 @@ def make_echo_server(create_empty_data=None):
     import numpy as np
 
     if create_empty_data is None:
-        create_empty_data = lambda size: np.empty(size, dtype=np.uint8)
+        create_empty_data = lambda n: np.empty(n, dtype=np.uint8)
 
     async def echo_server(ep):
         """
@@ -29,18 +30,26 @@ def make_echo_server(create_empty_data=None):
         """
         msg_size = np.empty(1, dtype=np.uint64)
         await ep.recv(msg_size)
-        msg = create_empty_data(msg_size)
+        msg = create_empty_data(msg_size[0])
         await ep.recv(msg)
         await ep.send(msg)
 
     return echo_server
 
 
+def handle_exception(loop, context):
+    # context["message"] will always be there; but context["exception"] may not
+    msg = context.get("exception", context["message"])
+    print(msg)
+    sys.exit(-1)
+
+
 @pytest.mark.asyncio
 @pytest.mark.parametrize("size", msg_sizes)
 @pytest.mark.parametrize("dtype", dtypes)
 async def test_send_recv_numpy(size, dtype):
-    np = pytest.importorskip("numpy")    
+    asyncio.get_running_loop().set_exception_handler(handle_exception)
+    np = pytest.importorskip("numpy")
 
     msg = np.arange(size, dtype=dtype)
     msg_size = np.array([msg.nbytes], dtype=np.uint64)
@@ -58,13 +67,14 @@ async def test_send_recv_numpy(size, dtype):
 @pytest.mark.parametrize("size", msg_sizes)
 @pytest.mark.parametrize("dtype", dtypes)
 async def test_send_recv_cupy(size, dtype):
+    asyncio.get_running_loop().set_exception_handler(handle_exception)
     np = pytest.importorskip("numpy")
     cupy = pytest.importorskip("cupy")
 
     msg = cupy.arange(size, dtype=dtype)
     msg_size = np.array([msg.nbytes], dtype=np.uint64)
 
-    listener = ucp.create_listener(make_echo_server(lambda size: cupy.empty(size, dtype=np.uint8)))
+    listener = ucp.create_listener(make_echo_server(lambda n: cupy.empty((n,), dtype=np.uint8)))
     client = await ucp.create_endpoint(ucp.get_address(), listener.port)
     await client.send(msg_size)
     await client.send(msg)
