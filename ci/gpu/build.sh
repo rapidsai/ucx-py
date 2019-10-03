2e89727f23981bd4f4883da066f1e1957090d292
Benjamin Zaitlen <quasiben@gmail.com>
2019-10-03 07:53:46 -0400

    use rapids nightly to get latest cudf for now

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -45,8 +45,8 @@ nvidia-smi
 
 logger "Activate conda env..."
 source activate gdf
-conda create install "cudf>=0.9" "dask-cudf>=0.9" "cudatoolkit=$CUDA_REL" \
-              "dask>=2.3.0" "distributed>=2.3.2" "numpy>=1.16"
+conda create install "cudf>=0.10" "dask-cudf>=0.10" "cudatoolkit=$CUDA_REL" \
+              "dask>=2.3.0" "distributed>=2.3.2" "numpy>=1.16" -c rapidsai-nightly
 
 # needed for asynccontextmanager in py36
 conda install -c conda-forge "async_generator" "automake" "libtool" \
