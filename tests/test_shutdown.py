ed048e8c11d402c3ec61176bff45a0b99adf2e43
Mads R. B. Kristensen <madsbk@gmail.com>
2019-10-07 10:49:12 +0200

    Added listener close test
    Based on @quasiben

--- tests/test_shutdown.py
+++ tests/test_shutdown.py
@@ -25,8 +25,8 @@ async def test_server_shutdown():
         with pytest.raises(ucp.exceptions.UCXCanceled):
             await ep.recv(msg)
 
-    lf = ucp.create_listener(server_node)
-    await client_node(lf.port)
+    listener = ucp.create_listener(server_node)
+    await client_node(listener.port)
 
 
 @pytest.mark.skipif(
@@ -47,5 +47,43 @@ async def test_client_shutdown():
         with pytest.raises(ucp.exceptions.UCXCanceled):
             await ep.recv(msg)
 
-    lf = ucp.create_listener(server_node)
-    await client_node(lf.port)
+    listener = ucp.create_listener(server_node)
+    await client_node(listener.port)
+
+
+@pytest.mark.asyncio
+async def test_listener_close():
+    """The server close the listener"""
+
+    async def client_node(port):
+        ep = await ucp.create_endpoint(ucp.get_address(), port)
+        await ep.send(np.arange(100, dtype=np.int64))
+        await ep.send(np.arange(100, dtype=np.int64))
+
+    async def server_node(ep):
+        msg = np.empty(100, dtype=np.int64)
+        await ep.recv(msg)
+        assert listener.closed == False
+        listener.close()
+        await ep.recv(msg)
+        assert listener.closed == True
+
+    listener = ucp.create_listener(server_node)
+    await client_node(listener.port)
+
+
+@pytest.mark.asyncio
+async def test_listener_del():
+    """The client delete the listener"""
+
+    async def server_node(ep):
+        msg = np.empty(100, dtype=np.int64)
+        await ep.recv(msg)
+        await ep.recv(msg)
+
+    listener = ucp.create_listener(server_node)
+    ep = await ucp.create_endpoint(ucp.get_address(), listener.port)
+    await ep.send(np.arange(100, dtype=np.int64))
+    assert listener.closed == False
+    del listener
+    await ep.send(np.arange(100, dtype=np.int64))
