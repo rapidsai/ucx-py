d045d7a3e538c79a24f04fbc83906efd6aced7a9
Benjamin Zaitlen <quasiben@gmail.com>
2019-10-07 07:53:25 -0400

    enforce style checks again

--- ci/checks/style.sh
+++ ci/checks/style.sh
@@ -55,5 +55,5 @@ RETVALS=($ISORT_RETVAL $BLACK_RETVAL $FLAKE_RETVAL)
 IFS=$'\n'
 RETVAL=`echo "${RETVALS[*]}" | sort -nr | head -n1`
 
-#exit $RETVAL
-exit 0 # don't force style checks yet 
+exit $RETVAL
+# exit 0 # don't force style checks yet
