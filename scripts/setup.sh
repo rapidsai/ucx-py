2bd899a79ba56d2bddaf429ba65feec2006a5d8a
John Kirkham <jakirkham@gmail.com>
2019-10-15 22:47:02 -0400

    Drop the `scripts` directory
    It appears this content was from the early days of ucx-py development
    and isn't particularly relevant now. Also looks like it is pretty
    out-of-date. Since this doesn't appear to be needed any more, go ahead
    and drop it.

--- scripts/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-module load cuda
-export LD_LIBRARY_PATH=/home/akvenkatesh/tools/install/lib64:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/home/akvenkatesh/tools/install/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/home/akvenkatesh/ucx-github/build/lib:$LD_LIBRARY_PATH
-export PATH=/home/akvenkatesh/tools/install/bin:$PATH
-export PYTHONPATH=/home/akvenkatesh/tools/install/lib/python3.7:$PYTHONPATH
-export UCX_PY_CUDA_PATH=/cm/extra/apps/CUDA.linux86-64/9.2.88.1_396.26
-export UCX_PY_UCX_PATH=/home/akvenkatesh/ucx-github/build
