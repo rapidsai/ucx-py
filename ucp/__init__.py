bad0c8cf3171620e3f4d91cc2981e50334532a4a
Benjamin Zaitlen <quasiben@gmail.com>
2019-11-21 08:21:54 -0500

    make hardcoded env vars overwriteable

--- ucp/__init__.py
+++ ucp/__init__.py
@@ -15,12 +15,12 @@ logger = logging.getLogger("ucx")
 
 # Notice, if we have to update environment variables
 # we need to do it before importing UCX
-if os.environ.get("UCX_MEMTYPE_CACHE", "") != "n":
+if "UCX_MEMTYPE_CACHE" not in os.environ:
     # See <https://github.com/openucx/ucx/wiki/NVIDIA-GPU-Support#known-issues>
     logger.debug("Setting env UCX_MEMTYPE_CACHE=n, which is required by UCX")
     os.environ["UCX_MEMTYPE_CACHE"] = "n"
 
-if os.environ.get("UCX_CUDA_IPC_CACHE", "") != "n":
+if "UCX_CUDA_IPC_CACHE" not in os.environ:
     # See <https://github.com/openucx/ucx/issues/4410>
     logger.debug(
         "Setting env UCX_CUDA_IPC_CACHE=n, which is required to avoid NVLink memory "
