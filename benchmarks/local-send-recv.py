79c01ef897a5e2e6fbb719f9f38ad11f72743099
Mads R. B. Kristensen <madsbk@gmail.com>
2019-11-21 05:14:14 -0500

    including individual iteration timings

--- benchmarks/local-send-recv.py
+++ benchmarks/local-send-recv.py
@@ -4,7 +4,7 @@ Benchmark send receive on one machine
 import argparse
 import asyncio
 import multiprocessing as mp
-from time import perf_counter as clock, sleep
+from time import perf_counter as clock
 
 from distributed.utils import format_bytes, parse_bytes
 
@@ -12,8 +12,6 @@ import numpy
 import ucp
 
 mp = mp.get_context("spawn")
-
-port = 4300
 
 
 def server(queue, args):
@@ -40,19 +38,14 @@ def server(queue, args):
 
             assert msg_recv_list[0].nbytes == args.n_bytes
 
-            start = clock()
             for i in range(args.n_iter):
                 await ep.recv(msg_recv_list[i], args.n_bytes)
                 await ep.send(msg_recv_list[i], args.n_bytes)
-            stop = clock()
-
-            took = stop - start
-            queue.put(took)
             await ep.close()
             lf.close()
 
-        lf = ucp.create_listener(server_handler, port)
-        host = ucp.get_address()
+        lf = ucp.create_listener(server_handler)
+        queue.put(lf.port)
 
         while not lf.closed():
             await asyncio.sleep(0.5)
@@ -62,7 +55,7 @@ def server(queue, args):
     loop.close()
 
 
-def client(queue, args):
+def client(queue, port, args):
     import ucp
 
     ucp.init()
@@ -92,14 +85,14 @@ def client(queue, args):
         assert msg_send_list[0].nbytes == args.n_bytes
         assert msg_recv_list[0].nbytes == args.n_bytes
 
-        start = clock()
+        times = []
         for i in range(args.n_iter):
+            start = clock()
             await ep.send(msg_send_list[i], args.n_bytes)
             await ep.recv(msg_recv_list[i], args.n_bytes)
-        stop = clock()
-
-        took = stop - start
-        queue.put(took)
+            stop = clock()
+            times.append(stop - start)
+        queue.put(times)
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(run())
@@ -175,36 +168,38 @@ def main():
     q1 = mp.Queue()
     p1 = mp.Process(target=server, args=(q1, args))
     p1.start()
-    sleep(1)
+    port = q1.get()
     q2 = mp.Queue()
-    p2 = mp.Process(target=client, args=(q2, args))
+    p2 = mp.Process(target=client, args=(q2, port, args))
     p2.start()
-    t1 = q1.get()
-    t2 = q2.get()
+    times = q2.get()
     p1.join()
     p2.join()
     assert not p1.exitcode
     assert not p2.exitcode
+    assert len(times) == args.n_iter
 
-    if args.object_type == "cupy":
-        gpu_dev = ["(GPU %d)" % args.server_dev, "(GPU %d)" % args.client_dev]
-    else:
-        gpu_dev = ["", ""]
-
+    print("Roundtrip benchmark")
     print("--------------------------")
-    print(f"n_iter   | {args.n_iter}")
-    print(f"n_bytes  | {format_bytes(args.n_bytes)}")
-    print(f"object   | {args.object_type}")
-    print("\n==========================")
-    print(
-        "Server%s: %s/s"
-        % (gpu_dev[0], format_bytes(2 * args.n_iter * args.n_bytes / t1))
-    )
-    print(
-        "Client%s: %s/s"
-        % (gpu_dev[1], format_bytes(2 * args.n_iter * args.n_bytes / t2))
-    )
+    print(f"n_iter      | {args.n_iter}")
+    print(f"n_bytes     | {format_bytes(args.n_bytes)}")
+    print(f"object      | {args.object_type}")
+    print(f"reuse alloc | {args.reuse_alloc}")
     print("==========================")
+    if args.object_type == "cupy":
+        print(f"Device      | {args.server_dev}, {args.client_dev}")
+    else:
+        print(f"Device      | CPU")
+    print(
+        f"Average     | {format_bytes(2 * args.n_iter * args.n_bytes / sum(times))}/s"
+    )
+    print("--------------------------")
+    print("Iterations")
+    print("--------------------------")
+    for i, t in enumerate(times):
+        ts = format_bytes(2 * args.n_bytes / t)
+        ts = (" " * (9 - len(ts))) + ts
+        print("%03d         |%s/s" % (i, ts))
 
 
 if __name__ == "__main__":
