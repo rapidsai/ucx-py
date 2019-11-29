ace97838e461e769e2bea40ef8a9ac04dbb9422d
Peter Andreas Entschev <peter@entschev.com>
2019-11-29 06:57:11 -0800

    Add TopologicalDistance docstrings

--- ucp/_libs/topological_distance.pyx
+++ ucp/_libs/topological_distance.pyx
@@ -11,13 +11,65 @@ cdef class TopologicalDistance:
         hwloc_topology_t topo
 
     def __init__(self):
+        """ Find topological distance between devices
+
+        To ensure best communication performance, it's important to identify devices
+        close to each other, allowing less communication overhead. Consider a system
+        with multiple InfiniBand interfaces, to maximize performance, each GPU should
+        choose the interface physically closer, thus allowing data to run through a
+        shortest path.
+
+        Currently, this class only supports finding the PCI network and openfabrics
+        (e.g., InfiniBand, aka. 'mlx5' or 'ib') interfaces closest to a NVIDIA GPU.
+
+        """
         self.topo = <hwloc_topology_t> initialize_topology()
 
     def __del__(self):
         cdef hwloc_topology_t topo = <hwloc_topology_t> self.topo
         hwloc_topology_destroy(topo)
 
-    def get_cuda_distances_from_pci_info(self, domain, bus, device, device_type="openfabrics"):
+    def get_cuda_distances_from_pci_info(self, domain, bus, device,
+            device_type="openfabrics"):
+        """ Find network or openfabrics devices closest to CUDA device at
+        domain:bus:device address.
+
+        Parameters
+        ----------
+        domain: int
+            PCI domain index of CUDA device
+        bus: int
+            PCI bus index of CUDA device
+        device: int
+            PCI device index of CUDA device
+        device_type: string
+            Type of device to find distance, currently supported types are "network"
+            and "openfabrics"
+
+        Returns
+        -------
+        List of distances and names to the CUDA device, sorted in ascending order of
+        distances (closest first). Note that there may be multiple devices with equal
+        distance, in such cases there's no particular ordering.
+
+        Example
+        -------
+        >>> import pynvml
+        >>> from ucp._libs.topological_distance import TopologicalDistance
+        >>> pynvml.nvmlInit()
+        >>> handle = pynvmlDeviceGetHandleByIndex(0)
+        >>> handle = pynvml.nvmlDeviceGetHandleByIndex(0)
+        >>> pci_info = pynvml.nvmlDeviceGetPciInfo(handle)
+        >>> domain, bus, device = pci_info.domain, pci_info.bus, pci_info.device
+        >>> td = TopologicalDistance()
+        >>> td.get_cuda_distances_from_device_index(domain, bus, device, "network")
+        [{'distance': 2, 'name': b'ib0'}, {'distance': 4, 'name': b'enp1s0f0'},
+         {'distance': 4, 'name': b'enp1s0f1'}, {'distance': 4, 'name': b'ib1'},
+         {'distance': 4, 'name': b'ib2'}, {'distance': 4, 'name': b'ib3'}]
+        >>> td.get_cuda_distances_from_device_index(domain, bus, device, "openfabrics")
+        [{'distance': 2, 'name': b'mlx5_0'}, {'distance': 4, 'name': b'mlx5_1'},
+         {'distance': 4, 'name': b'mlx5_2'}, {'distance': 4, 'name': b'mlx5_3'}]
+        """
         if device_type == "openfabrics":
             hwloc_osdev_type = HWLOC_OBJ_OSDEV_OPENFABRICS
         if device_type == "network":
@@ -47,6 +99,34 @@ cdef class TopologicalDistance:
         return ret
 
     def get_cuda_distances_from_device_index(self, cuda_device_index, device_type="openfabrics"):
+        """ Find network or openfabrics devices closest to CUDA device of given index.
+
+        Parameters
+        ----------
+        cuda_device_index: int
+            Index of the CUDA device
+        device_type: string
+            Type of device to find distance, currently supported types are "network"
+            and "openfabrics"
+
+        Returns
+        -------
+        List of distances and names to the CUDA device, sorted in ascending order of
+        distances (closest first). Note that there may be multiple devices with equal
+        distance, in such cases there's no particular ordering.
+
+        Example
+        -------
+        >>> from ucp._libs.topological_distance import TopologicalDistance
+        >>> td = TopologicalDistance()
+        >>> td.get_cuda_distances_from_device_index(0, "network")
+        [{'distance': 2, 'name': b'ib0'}, {'distance': 4, 'name': b'enp1s0f0'},
+         {'distance': 4, 'name': b'enp1s0f1'}, {'distance': 4, 'name': b'ib1'},
+         {'distance': 4, 'name': b'ib2'}, {'distance': 4, 'name': b'ib3'}]
+        >>> td.get_cuda_distances_from_device_index(0, "openfabrics")
+        [{'distance': 2, 'name': b'mlx5_0'}, {'distance': 4, 'name': b'mlx5_1'},
+         {'distance': 4, 'name': b'mlx5_2'}, {'distance': 4, 'name': b'mlx5_3'}]
+        """
         pynvml.nvmlInit()
         handle = pynvml.nvmlDeviceGetHandleByIndex(cuda_device_index)
         pci_info = pynvml.nvmlDeviceGetPciInfo(handle)
