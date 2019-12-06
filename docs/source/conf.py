86a64289d2b1dbfe776e40b0604052bdfeef4210
Raymond Douglass <ray@raydouglass.com>
2019-12-05 19:41:12 -0500

    DOC Update to v0.11

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = "2019, NVIDIA"
 author = "NVIDIA"
 
 # The short X.Y version
-version = ""
+version = "0.11"
 # The full version, including alpha/beta/rc tags
-release = ""
+release = "0.11.0a"
 
 
 # -- General configuration ---------------------------------------------------
