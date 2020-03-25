60f7bbac59f0a63a2203eba06e523439ed1c7f9f
John Kirkham <jakirkham@gmail.com>
2020-03-24 18:04:32 -0700

    Space out `import`s from code

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ import versioneer
 from setuptools import setup
 from setuptools.extension import Extension
 
+
 include_dirs = [os.path.dirname(get_python_inc())]
 library_dirs = [get_config_var("LIBDIR")]
 libraries = ["ucp", "uct", "ucm", "ucs", "hwloc"]
