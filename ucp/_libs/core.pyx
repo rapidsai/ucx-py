b255aa293badb5046e7a50b0524e99e15477b6fd
Benjamin Zaitlen <quasiben@gmail.com>
2020-04-02 07:38:11 -0700

    remove pass and move comment

--- ucp/_libs/core.pyx
+++ ucp/_libs/core.pyx
@@ -422,10 +422,10 @@ class _Endpoint:
                     self._ctrl_tag_send,
                     pending_msg=self.pending_msg_list[-1]
                 )
+            # The peer might already be shutting down
             except UCXError as e:
                 log = "UCX Closing Error on worker %d\n%s" % (hex(self.uid), str(e))
                 logging.error(log)
-                pass  # The peer might already be shutting down
             else:
                 # Only if there were no errors closing peer
                 # Give all current outstanding send() calls a chance to return
