3e8c1c0b90789a7c185d5cf075fff9113e2b037c
Mads R. B. Kristensen <madsbk@gmail.com>
2020-04-14 10:07:43 +0200

    cleanup

--- tests/test_guarantee_msg_order.py
+++ tests/test_guarantee_msg_order.py
@@ -3,18 +3,17 @@ import ucp
 
 
 @pytest.mark.asyncio
-async def test_mismatch():
+@pytest.mark.parametrize("server_guarantee_msg_order", [True, False])
+async def test_mismatch(server_guarantee_msg_order):
     def server(ep):
         pass
 
-    lt = ucp.create_listener(server, guarantee_msg_order=True)
+    lt = ucp.create_listener(server, guarantee_msg_order=server_guarantee_msg_order)
     with pytest.raises(
         ValueError, match="Both peers must set guarantee_msg_order identically"
     ):
-        await ucp.create_endpoint(ucp.get_address(), lt.port, guarantee_msg_order=False)
-
-    lt = ucp.create_listener(server, guarantee_msg_order=False)
-    with pytest.raises(
-        ValueError, match="Both peers must set guarantee_msg_order identically"
-    ):
-        await ucp.create_endpoint(ucp.get_address(), lt.port, guarantee_msg_order=True)
+        await ucp.create_endpoint(
+            ucp.get_address(),
+            lt.port,
+            guarantee_msg_order=(not server_guarantee_msg_order),
+        )
