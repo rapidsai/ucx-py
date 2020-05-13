5a1d95e5fa9b8e5afafefc1174602d6079f010d8
Benjamin Zaitlen <quasiben@gmail.com>
2020-05-13 15:24:58 -0700

    lint

--- tests/debug-tests/scheduler.sh
+++ tests/debug-tests/scheduler.sh
@@ -8,4 +8,3 @@ export UCX_TLS=tcp,sockcm,cuda_copy,rc,cuda_ipc
 export UCX_SOCKADDR_TLS_PRIORITY=sockcm
 
 UCX_NET_DEVICES=mlx5_0:1 CUDA_VISIBLE_DEVICES=0 python send.py 2>&1 | tee /tmp/send-log.txt &
-
