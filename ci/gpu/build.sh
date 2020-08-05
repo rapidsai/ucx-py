7a4feef3fd56d3dc2eaa574b87a2f7fa4a3b33c9
John Kirkham <jakirkham@gmail.com>
2020-08-04 19:32:05 -0700

    Drop `cmake`
    UCX does not need `cmake` to build and `ucx-py` doesn't use it either.
    So just drop `cmake` from CI.

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -53,7 +53,7 @@ conda install "cudatoolkit=$CUDA_REL" \
 
 # needed for asynccontextmanager in py36
 conda install -c conda-forge "async_generator" "automake" "libtool" \
-                              "cmake" "automake" "autoconf" "cython>=0.29.14,<3.0.0a0" \
+                              "automake" "autoconf" "cython>=0.29.14,<3.0.0a0" \
                               "pytest" "pkg-config" "pytest-asyncio" \
                               "pynvml" "libhwloc" "psutil"
 
