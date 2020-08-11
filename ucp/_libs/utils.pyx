376df5c9cc865f0bd57923cd071ad6659f3cb5d5
John Kirkham <jakirkham@gmail.com>
2020-08-10 21:24:14 -0700

    Space out code from docstring

--- ucp/_libs/utils.pyx
+++ ucp/_libs/utils.pyx
@@ -16,6 +16,7 @@ def get_buffer_data(buffer, check_writable=False):
     Returns data pointer of the buffer. Raising ValueError if the buffer
     is read only and check_writable=True is set.
     """
+
     iface = None
     if hasattr(buffer, "__cuda_array_interface__"):
         iface = buffer.__cuda_array_interface__
