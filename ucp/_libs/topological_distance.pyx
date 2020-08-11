3021780c89fef8f1ae239d3f68dee4e3c0f6fa5d
John Kirkham <jakirkham@gmail.com>
2020-08-11 16:05:23 -0700

    Split out directives from license comment

--- ucp/_libs/topological_distance.pyx
+++ ucp/_libs/topological_distance.pyx
@@ -1,5 +1,6 @@
 # Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
 # See file LICENSE for terms.
+
 # cython: language_level=3
 
 import pynvml
--- ucp/_libs/ucx_api.pyx
+++ ucp/_libs/ucx_api.pyx
@@ -1,6 +1,8 @@
 # Copyright (c) 2019-2020, NVIDIA CORPORATION. All rights reserved.
 # See file LICENSE for terms.
+
 # cython: language_level=3
+
 import logging
 import socket
 import weakref
