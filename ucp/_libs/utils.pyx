2b0d5142abef3bf03699816b3f4dcf4bb63ef2ae
John Kirkham <jakirkham@gmail.com>
2020-08-10 22:15:03 -0700

    Drop old Numba workaround
    This got fixed in Numba 0.46+ and RAPIDS requires an even newer version
    of Numba. So we are safe to drop this workaround.

--- ucp/_libs/utils.pyx
+++ ucp/_libs/utils.pyx
@@ -31,11 +31,6 @@ def get_buffer_data(buffer, check_writable=False):
         data_ptr = int(<uintptr_t>PyMemoryView_GET_BUFFER(mview).buf)
         data_readonly = mview.readonly
 
-    # Workaround for numba giving None, rather than an 0.
-    # https://github.com/cupy/cupy/issues/2104 for more info.
-    if data_ptr is None:
-        data_ptr = 0
-
     if data_ptr == 0:
         raise NotImplementedError("zero-sized buffers isn't supported")
 
