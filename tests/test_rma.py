4beb784a640e578854ff8f4cf1c7bd98faf600b9
Matthew Baker <bakermb@ornl.gov>
2020-09-04 16:14:48 -0400

    Improve tests.

--- tests/test_rma.py
+++ tests/test_rma.py
@@ -19,9 +19,11 @@ async def test_flush(blocking_progress_mode):
     await ucp.flush()
     ucp.reset()
 
+
 @pytest.mark.parametrize("blocking_progress_mode", [True, False])
 def test_worker_address(blocking_progress_mode):
     ucp.init(blocking_progress_mode=blocking_progress_mode)
 
     addr = ucp.get_worker_address()
+    assert addr is not None
     ucp.reset()
