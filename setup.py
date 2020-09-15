d05c678481d9a77c9d0305416dc5f86cd15d640f
John Kirkham <jakirkham@gmail.com>
2020-09-14 23:59:01 -0700

    Drop internal Cython `utils`
    As this has now been absorbed into `Array`, we can drop these utility
    functions.

--- setup.py
+++ setup.py
@@ -47,14 +47,6 @@ ext_modules = [
         extra_compile_args=extra_compile_args,
     ),
     Extension(
-        "ucp._libs.utils",
-        sources=["ucp/_libs/utils.pyx"],
-        include_dirs=include_dirs,
-        library_dirs=library_dirs,
-        libraries=libraries,
-        extra_compile_args=extra_compile_args,
-    ),
-    Extension(
         "ucp._libs.topological_distance",
         sources=[
             "ucp/_libs/topological_distance.pyx",
