d237cf80da8449e4569beec094963ed209f063d6
John Kirkham <jakirkham@gmail.com>
2020-10-06 20:30:23 -0700

    Clarify disconnect test
    Try to provide some more context about why this disconnect test may
    generate errors and what it is actually testing. Namely that the
    processes exit cleanly.

--- tests/test_disconnect.py
+++ tests/test_disconnect.py
@@ -20,7 +20,7 @@ async def mp_queue_get_nowait(queue):
         await asyncio.sleep(0.01)
 
 
-def _test_shutdown_closed_peer_server(client_queue, server_queue):
+def _test_shutdown_unexpected_closed_peer_server(client_queue, server_queue):
     async def run():
         async def server_node(ep):
             try:
@@ -43,7 +43,7 @@ def _test_shutdown_closed_peer_server(client_queue, server_queue):
     assert log.find("""UCXError('<[Send shutdown]""") != -1
 
 
-def _test_shutdown_closed_peer_client(client_queue, server_queue):
+def _test_shutdown_unexpected_closed_peer_client(client_queue, server_queue):
     async def run():
         server_port = client_queue.get()
         ep = await ucp.create_endpoint(ucp.get_address(), server_port)
@@ -53,15 +53,25 @@ def _test_shutdown_closed_peer_client(client_queue, server_queue):
     asyncio.get_event_loop().run_until_complete(run())
 
 
-def test_shutdown_closed_peer(caplog):
+def test_shutdown_unexpected_closed_peer(caplog):
+    """
+    Test clean client shutdown after unexpected peer close
+
+    This will causes some UCX warnings to be issued, but this as expected.
+    The main goal is to assert that the processes exit without errors
+    despite a somewhat messy initial state.
+    """
+
     client_queue = mp.Queue()
     server_queue = mp.Queue()
     p1 = mp.Process(
-        target=_test_shutdown_closed_peer_server, args=(client_queue, server_queue)
+        target=_test_shutdown_unexpected_closed_peer_server,
+        args=(client_queue, server_queue),
     )
     p1.start()
     p2 = mp.Process(
-        target=_test_shutdown_closed_peer_client, args=(client_queue, server_queue)
+        target=_test_shutdown_unexpected_closed_peer_client,
+        args=(client_queue, server_queue),
     )
     p2.start()
     p2.join()
