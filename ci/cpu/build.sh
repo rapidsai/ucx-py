22c6eae9bdae628f69393987f3b9681d9f5af70a
Mark Sadang <msadang@nvidia.com>
2020-11-06 13:37:20 -0800

    Gpuciscripts clean and update

--- ci/cpu/build.sh
+++ ci/cpu/build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-# Copyright (c) 2018, NVIDIA CORPORATION.
+# Copyright (c) 2020, NVIDIA CORPORATION.
 ######################################
 # ucx-py CPU conda build script for CI #
 ######################################
 set -e
 
 # Logger function for build status output
-function logger() {
+function gpuci_logger() {
   echo -e "\n>>>> $@\n"
 }
 
 # Set path and build parallel level
-export PATH=/conda/bin:/usr/local/cuda/bin:$PATH
-export PARALLEL_LEVEL=4
+export PATH=/opt/conda/bin:/usr/local/cuda/bin:$PATH
+export PARALLEL_LEVEL=-4
 
 # Set home to the job's workspace
 export HOME=$WORKSPACE
@@ -28,17 +28,20 @@ export GIT_DESCRIBE_NUMBER=`git rev-list ${GIT_DESCRIBE_TAG}..HEAD --count`
 # SETUP - Check environment
 ################################################################################
 
-logger "Get env..."
+gpuci_logger "Get env"
 env
 
-logger "Activate conda env..."
-source activate gdf
+gpuci_logger "Activate conda env"
+. /opt/conda/etc/profile.d/conda.sh
+conda activate rapids
 
-logger "Check versions..."
+gpuci_logger "Check versions"
 python --version
-gcc --version
-g++ --version
-conda list
+$CC --version
+$CXX --version
+conda info
+conda config --show-sources
+conda list --show-channel-urls
 
 # FIX Added to deal with Anancoda SSL verification issues during conda builds
 conda config --set ssl_verify False
@@ -47,12 +50,12 @@ conda config --set ssl_verify False
 # BUILD - Conda package builds (conda deps: ucx-py)
 ################################################################################
 
-# logger "Build conda pkg for ucx-py..."
+# logger "Build conda pkg for ucx-py"
 # source ci/cpu/ucx-py/build_ucx-py.sh
 
 ################################################################################
 # UPLOAD - Conda packages
 ################################################################################
 
-# logger "Upload conda pkgs..."
+# logger "Upload conda pkgs"
 # source ci/cpu/upload_anaconda.sh
