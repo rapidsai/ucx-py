627466d18f43200aee867bde0dfc32696b0dae96
Peter Andreas Entschev <peter@entschev.com>
2021-03-04 13:54:10 -0800

    Adjust number of clients in test_single_process

--- tests/test_single_process.py
+++ tests/test_single_process.py
@@ -29,33 +29,22 @@ async def client_node(port):
 
 
 @pytest.mark.asyncio
-async def test_multiple_nodes():
-    lf1 = ucp.create_listener(server_node)
-    lf2 = ucp.create_listener(server_node)
-    assert lf1.port != lf2.port
-
-    nodes = []
-    for _ in range(10):
-        nodes.append(client_node(lf1.port))
-        nodes.append(client_node(lf2.port))
-    await asyncio.gather(*nodes, loop=asyncio.get_event_loop())
-
-
-@pytest.mark.asyncio
-async def test_one_server_many_clients():
+async def test_one_listener_many_clients():
     lf = ucp.create_listener(server_node)
     clients = []
-    for _ in range(100):
+    for _ in range(50):
         clients.append(client_node(lf.port))
     await asyncio.gather(*clients, loop=asyncio.get_event_loop())
 
 
 @pytest.mark.asyncio
-async def test_two_servers_many_clients():
+async def test_two_listeners_many_clients():
     lf1 = ucp.create_listener(server_node)
     lf2 = ucp.create_listener(server_node)
+    assert lf1.port != lf2.port
+
     clients = []
-    for _ in range(100):
+    for _ in range(25):
         clients.append(client_node(lf1.port))
         clients.append(client_node(lf2.port))
     await asyncio.gather(*clients, loop=asyncio.get_event_loop())
