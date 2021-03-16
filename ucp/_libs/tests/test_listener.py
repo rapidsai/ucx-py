8b656674d29bbd6c448b4dbc9f823ea22b946cb7
Mads R. B. Kristensen <madsbk@gmail.com>
2021-03-16 16:09:18 +0100

    minor clean up

--- ucp/_libs/tests/test_listener.py
+++ ucp/_libs/tests/test_listener.py
@@ -1,14 +1,14 @@
-from ucp._libs.ucx_api import UCXContext, UCXListener, UCXWorker
+from ucp._libs import ucx_api
 
 
 def test_listener_ip_port():
+    ctx = ucx_api.UCXContext()
+    worker = ucx_api.UCXWorker(ctx)
+
     def _listener_handler(conn_request, callback_func):
         pass
 
-    context = UCXContext({})
-    worker = UCXWorker(context)
-
-    listener = UCXListener(
+    listener = ucx_api.UCXListener(
         worker=worker, port=0, cb_func=_listener_handler, cb_args=(lambda: None,)
     )
 
