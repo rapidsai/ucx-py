aaffb01a68410679d64b23c5873175816f67cff3
Peter Andreas Entschev <peter@entschev.com>
2021-04-26 10:21:26 +0200

    Update ucx_api.pyi stub (#714)
    * Update ucx_api.pyi stub

--- ucp/_libs/ucx_api.pyi
+++ ucp/_libs/ucx_api.pyi
@@ -1,10 +1,12 @@
 import enum
-from typing import Callable, Iterable, Mapping, Optional
+from typing import Callable, Dict, Iterable, Mapping, Optional, Tuple
 
-def get_current_options() -> None: ...
+# typedefs.pyx
 
-class UCXObject:
-    def close(self) -> None: ...
+class AllocatorType(enum.Enum):
+    HOST: int
+    CUDA: int
+    UNSUPPORTED: int
 
 class Feature(enum.Enum):
     TAG: int
@@ -15,10 +17,25 @@ class Feature(enum.Enum):
     STREAM: int
     AM: int
 
+# utils.pyx
+
+def get_current_options() -> Dict[str, str]: ...
+def get_ucx_version() -> Tuple[int]: ...
+def is_am_supported() -> bool: ...
+
+# ucx_object.pyx
+
+class UCXObject:
+    def close(self) -> None: ...
+
+# ucx_context.pyx
+
 class UCXContext(UCXObject):
     def __init__(
         self, config_dict: Mapping = ..., feature_flags: Iterable[Feature] = ...
     ): ...
+
+# ucx_address.pyx
 
 class UCXAddress:
     @classmethod
@@ -29,6 +46,8 @@ class UCXAddress:
     def address(self) -> int: ...
     @property
     def length(self) -> int: ...
+
+# ucx_worker.pyx
 
 class UCXWorker(UCXObject):
     def __init__(self, context: UCXContext): ...
@@ -42,6 +61,11 @@ class UCXWorker(UCXObject):
     def ep_create_from_conn_request(
         self, conn_request: int, endpoint_error_handling: bool
     ) -> UCXEndpoint: ...
+    def register_am_allocator(
+        self, allocator: Callable, allocator_type: AllocatorType
+    ) -> None: ...
+
+# ucx_listener.pyx
 
 class UCXListener(UCXObject):
     port: int
@@ -55,10 +79,24 @@ class UCXListener(UCXObject):
         cb_kwargs: dict = ...,
     ): ...
 
+# ucx_endpoint.pyx
+
 class UCXEndpoint(UCXObject):
     def info(self) -> str: ...
     @property
     def worker(self) -> UCXWorker: ...
+    def unpack_rkey(self, rkey) -> UCXRkey: ...
+
+# ucx_memory_handle.pyx
+
+class UCXMemoryHandle(UCXObject):
+    @classmethod
+    def alloc(cls, ctx: UCXContext, size: int) -> UCXMemoryHandle: ...
+    @classmethod
+    def map(cls, ctx: UCXContext, buffer) -> UCXMemoryHandle: ...
+    def pack_rkey(self) -> PackedRemoteKey: ...
+
+# transfer.pyx
 
 def tag_send_nb(
     ep: UCXEndpoint,
@@ -94,6 +132,22 @@ def stream_recv_nb(
     ep: UCXEndpoint,
     buffer,
     nbytes: int,
+    cb_func: Callable,
+    cb_args: Optional[tuple] = ...,
+    cb_kwargs: Optional[dict] = ...,
+    name: Optional[str] = ...,
+): ...
+def am_send_nbx(
+    ep: UCXEndpoint,
+    buffer,
+    nbytes: int,
+    cb_func: Callable,
+    cb_args: Optional[tuple] = ...,
+    cb_kwargs: Optional[dict] = ...,
+    name: Optional[str] = ...,
+): ...
+def am_recv_nb(
+    ep: UCXEndpoint,
     cb_func: Callable,
     cb_args: Optional[tuple] = ...,
     cb_kwargs: Optional[dict] = ...,
