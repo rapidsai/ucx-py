391b7c2b1f8ef19c93ded64bc90e4d99ba184ee9
Peter Andreas Entschev <peter@entschev.com>
2021-04-28 11:08:20 +0200

    Add ucx_api tests for AM (#718)
    * Test core for AM config
    
    * Test core for AM send/recv
    
    * Skip core AM tests in UCX < 1.10

--- ucp/_libs/tests/test_config.py
+++ ucp/_libs/tests/test_config.py
@@ -41,14 +41,19 @@ def test_init_invalid_option():
         ucx_api.UCXContext(options)
 
 
-@pytest.mark.parametrize("feature_flag", [ucx_api.Feature.TAG, ucx_api.Feature.STREAM])
+@pytest.mark.parametrize(
+    "feature_flag", [ucx_api.Feature.TAG, ucx_api.Feature.STREAM, ucx_api.Feature.AM]
+)
 def test_feature_flags_mismatch(feature_flag):
+    if feature_flag == ucx_api.Feature.AM and not ucx_api.is_am_supported():
+        pytest.skip("AM only supported in UCX >= 1.11")
+
     ctx = ucx_api.UCXContext(feature_flags=(feature_flag,))
     worker = ucx_api.UCXWorker(ctx)
     addr = worker.get_address()
     ep = worker.ep_create_from_worker_address(addr, endpoint_error_handling=False)
     msg = Array(bytearray(10))
-    if feature_flag == ucx_api.Feature.STREAM:
+    if feature_flag != ucx_api.Feature.TAG:
         with pytest.raises(
             ValueError, match="UCXContext must be created with `Feature.TAG`"
         ):
@@ -57,7 +62,7 @@ def test_feature_flags_mismatch(feature_flag):
             ValueError, match="UCXContext must be created with `Feature.TAG`"
         ):
             ucx_api.tag_recv_nb(worker, msg, msg.nbytes, 0, None)
-    elif feature_flag == ucx_api.Feature.TAG:
+    if feature_flag != ucx_api.Feature.STREAM:
         with pytest.raises(
             ValueError, match="UCXContext must be created with `Feature.STREAM`"
         ):
@@ -66,3 +71,12 @@ def test_feature_flags_mismatch(feature_flag):
             ValueError, match="UCXContext must be created with `Feature.STREAM`"
         ):
             ucx_api.stream_recv_nb(ep, msg, msg.nbytes, None)
+    if feature_flag != ucx_api.Feature.AM and ucx_api.is_am_supported():
+        with pytest.raises(
+            ValueError, match="UCXContext must be created with `Feature.AM`"
+        ):
+            ucx_api.am_send_nbx(ep, msg, msg.nbytes, None)
+        with pytest.raises(
+            ValueError, match="UCXContext must be created with `Feature.AM`"
+        ):
+            ucx_api.am_recv_nb(ep, None)
