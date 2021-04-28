51d11c6a0b6f1294c2f74d318386de9523ecb0b3
Peter Andreas Entschev <peter@entschev.com>
2021-04-28 11:31:42 +0200

    Split transfer and UCXWorker callback functions into multiple files (#716)
    * Split transfer functions into common, AM, Stream, TAG files
    
    * Move UCXWorker callback functions to new file

---
+++ ucp/_libs/transfer_common.pyx
@@ -0,0 +1,57 @@
+# Copyright (c) 2019-2020, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2020       UT-Battelle, LLC. All rights reserved.
+# See file LICENSE for terms.
+
+# cython: language_level=3
+
+from libc.stdint cimport uintptr_t
+
+from .ucx_api_dep cimport *
+
+from ..exceptions import UCXCanceled, UCXError, log_errors
+
+
+# This callback function is currently needed by stream_send_nb and
+# tag_send_nb transfer functions, as well as UCXEndpoint and UCXWorker
+# flush methods.
+cdef void _send_callback(void *request, ucs_status_t status):
+    cdef UCXRequest req
+    cdef dict req_info
+    cdef str name, ucx_status_msg, msg
+    cdef set inflight_msgs
+    cdef tuple cb_args
+    cdef dict cb_kwargs
+    with log_errors():
+        req = UCXRequest(<uintptr_t><void*> request)
+        assert not req.closed()
+        req_info = <dict>req._handle.info
+        req_info["status"] = "finished"
+
+        if "cb_func" not in req_info:
+            # This callback function was called before ucp_tag_send_nb() returned
+            return
+
+        exception = None
+        if status == UCS_ERR_CANCELED:
+            name = req_info["name"]
+            msg = "<%s>: " % name
+            exception = UCXCanceled(msg)
+        elif status != UCS_OK:
+            name = req_info["name"]
+            ucx_status_msg = ucs_status_string(status).decode("utf-8")
+            msg = "<%s>: %s" % (name, ucx_status_msg)
+            exception = UCXError(msg)
+        try:
+            inflight_msgs = req_info["inflight_msgs"]
+            inflight_msgs.discard(req)
+            cb_func = req_info["cb_func"]
+            if cb_func is not None:
+                cb_args = req_info["cb_args"]
+                if cb_args is None:
+                    cb_args = ()
+                cb_kwargs = req_info["cb_kwargs"]
+                if cb_kwargs is None:
+                    cb_kwargs = {}
+                cb_func(req, exception, *cb_args, **cb_kwargs)
+        finally:
+            req.close()
