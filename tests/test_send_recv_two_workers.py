58f62db61e3e2a60de791128dde1d1d88f5dcb8c
Peter Andreas Entschev <peter@entschev.com>
2021-04-30 14:20:57 -0700

    Fix AM skip condition in two workers test

--- tests/test_send_recv_two_workers.py
+++ tests/test_send_recv_two_workers.py
@@ -191,7 +191,7 @@ def cupy_obj():
 )
 @pytest.mark.parametrize("comm_api", ["tag", "am"])
 def test_send_recv_cu(cuda_obj_generator, comm_api):
-    if not ucp._libs.ucx_api.is_am_supported():
+    if comm_api == "am" and not ucp._libs.ucx_api.is_am_supported():
         pytest.skip("AM only supported in UCX >= 1.11")
 
     base_env = os.environ
