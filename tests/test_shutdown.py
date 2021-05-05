e9b33dfa066d118beabf72d4896726acdb147471
Peter Andreas Entschev <peter@entschev.com>
2021-05-05 13:14:05 -0700

    Skip shutdown tests with error handling on UCX < 1.10

--- tests/test_shutdown.py
+++ tests/test_shutdown.py
@@ -33,6 +33,8 @@ async def shutdown(ep):
 @pytest.mark.parametrize("endpoint_error_handling", [False, True])
 async def test_server_shutdown(endpoint_error_handling):
     """The server calls shutdown"""
+    if ucp.get_ucx_version() < (1, 10, 0) and endpoint_error_handling is True:
+        pytest.skip("Endpoint error handling is only supported for UCX >= 1.10")
 
     async def server_node(ep):
         msg = np.empty(10 ** 6)
@@ -41,13 +43,15 @@ async def test_server_shutdown(endpoint_error_handling):
 
     async def client_node(port):
         ep = await ucp.create_endpoint(
-            ucp.get_address(), port, endpoint_error_handling=True
+            ucp.get_address(), port, endpoint_error_handling=endpoint_error_handling
         )
         msg = np.empty(10 ** 6)
         with pytest.raises(ucp.exceptions.UCXCanceled):
             await ep.recv(msg)
 
-    listener = ucp.create_listener(server_node, endpoint_error_handling=True)
+    listener = ucp.create_listener(
+        server_node, endpoint_error_handling=endpoint_error_handling
+    )
     await client_node(listener.port)
 
 
@@ -58,10 +62,12 @@ async def test_server_shutdown(endpoint_error_handling):
 @pytest.mark.parametrize("endpoint_error_handling", [False, True])
 async def test_client_shutdown(endpoint_error_handling):
     """The client calls shutdown"""
+    if ucp.get_ucx_version() < (1, 10, 0) and endpoint_error_handling is True:
+        pytest.skip("Endpoint error handling is only supported for UCX >= 1.10")
 
     async def client_node(port):
         ep = await ucp.create_endpoint(
-            ucp.get_address(), port, endpoint_error_handling=True
+            ucp.get_address(), port, endpoint_error_handling=endpoint_error_handling
         )
         msg = np.empty(10 ** 6)
         with pytest.raises(ucp.exceptions.UCXCanceled):
@@ -72,7 +78,9 @@ async def test_client_shutdown(endpoint_error_handling):
         with pytest.raises(ucp.exceptions.UCXCanceled):
             await ep.recv(msg)
 
-    listener = ucp.create_listener(server_node, endpoint_error_handling=True)
+    listener = ucp.create_listener(
+        server_node, endpoint_error_handling=endpoint_error_handling
+    )
     await client_node(listener.port)
 
 
@@ -80,6 +88,8 @@ async def test_client_shutdown(endpoint_error_handling):
 @pytest.mark.parametrize("endpoint_error_handling", [False, True])
 async def test_listener_close(endpoint_error_handling):
     """The server close the listener"""
+    if ucp.get_ucx_version() < (1, 10, 0) and endpoint_error_handling is True:
+        pytest.skip("Endpoint error handling is only supported for UCX >= 1.10")
 
     async def client_node(listener):
         ep = await ucp.create_endpoint(
@@ -112,6 +122,8 @@ async def test_listener_close(endpoint_error_handling):
 @pytest.mark.parametrize("endpoint_error_handling", [False, True])
 async def test_listener_del(endpoint_error_handling):
     """The client delete the listener"""
+    if ucp.get_ucx_version() < (1, 10, 0) and endpoint_error_handling is True:
+        pytest.skip("Endpoint error handling is only supported for UCX >= 1.10")
 
     async def server_node(ep):
         await ep.send(np.arange(100, dtype=np.int64))
@@ -140,6 +152,8 @@ async def test_listener_del(endpoint_error_handling):
 @pytest.mark.parametrize("endpoint_error_handling", [False, True])
 async def test_close_after_n_recv(endpoint_error_handling):
     """The Endpoint.close_after_n_recv()"""
+    if ucp.get_ucx_version() < (1, 10, 0) and endpoint_error_handling is True:
+        pytest.skip("Endpoint error handling is only supported for UCX >= 1.10")
 
     async def server_node(ep):
         for _ in range(10):
