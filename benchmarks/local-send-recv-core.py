5acc8c70e20d0d8670f0d806b1834628277a6869
Peter Andreas Entschev <peter@entschev.com>
2021-06-01 03:20:18 -0700

    Add support for AM in local-send-recv-core benchmark

--- benchmarks/local-send-recv-core.py
+++ benchmarks/local-send-recv-core.py
@@ -27,26 +27,52 @@ from distributed.utils import format_bytes, parse_bytes
 
 import ucp
 from ucp._libs import ucx_api
-from ucp._libs.utils_test import blocking_recv, blocking_send
+from ucp._libs.utils_test import (
+    blocking_am_recv,
+    blocking_am_send,
+    blocking_recv,
+    blocking_send,
+)
 
 mp = mp.get_context("spawn")
+
+
+def register_am_allocators(args, worker):
+    if not args.enable_am:
+        return
+
+    import numpy as np
+
+    worker.register_am_allocator(
+        lambda n: np.empty(n, dtype=np.uint8), ucx_api.AllocatorType.HOST
+    )
+
+    if args.object_type == "cupy":
+        import cupy as cp
+
+        worker.register_am_allocator(
+            lambda n: cp.empty(n, dtype=cp.uint8), ucx_api.AllocatorType.CUDA
+        )
+    elif args.object_type == "rmm":
+        import rmm
+
+        worker.register_am_allocator(
+            lambda n: rmm.DeviceBuffer(size=n), ucx_api.AllocatorType.CUDA
+        )
 
 
 def server(queue, args):
     if args.server_cpu_affinity >= 0:
         os.sched_setaffinity(0, [args.server_cpu_affinity])
 
-    ctx = ucx_api.UCXContext(feature_flags=(ucx_api.Feature.TAG,))
-    worker = ucx_api.UCXWorker(ctx)
-
     if args.object_type == "numpy":
-        import numpy as np
+        import numpy as xp
     elif args.object_type == "cupy":
-        import cupy as np
+        import cupy as xp
 
-        np.cuda.runtime.setDevice(args.server_dev)
+        xp.cuda.runtime.setDevice(args.server_dev)
     else:
-        import cupy as np
+        import cupy as xp
 
         import rmm
 
@@ -56,8 +82,17 @@ def server(queue, args):
             initial_pool_size=args.rmm_init_pool_size,
             devices=[args.server_dev],
         )
-        np.cuda.runtime.setDevice(args.server_dev)
-        np.cuda.set_allocator(rmm.rmm_cupy_allocator)
+        xp.cuda.runtime.setDevice(args.server_dev)
+        xp.cuda.set_allocator(rmm.rmm_cupy_allocator)
+
+    ctx = ucx_api.UCXContext(
+        feature_flags=(
+            ucx_api.Feature.AM if args.enable_am is True else ucx_api.Feature.TAG,
+        )
+    )
+    worker = ucx_api.UCXWorker(ctx)
+
+    register_am_allocators(args, worker)
 
     finished = [False]
 
@@ -66,23 +101,31 @@ def server(queue, args):
             worker, conn_request, endpoint_error_handling=True
         )
 
-        msg_recv_list = []
-        if not args.reuse_alloc:
-            for _ in range(args.n_iter):
-                msg_recv_list.append(np.zeros(args.n_bytes, dtype="u1"))
-        else:
-            t = np.zeros(args.n_bytes, dtype="u1")
-            for _ in range(args.n_iter):
-                msg_recv_list.append(t)
+        if not args.enable_am:
+            msg_recv_list = []
+            if not args.reuse_alloc:
+                for _ in range(args.n_iter):
+                    msg_recv_list.append(xp.zeros(args.n_bytes, dtype="u1"))
+            else:
+                t = xp.zeros(args.n_bytes, dtype="u1")
+                for _ in range(args.n_iter):
+                    msg_recv_list.append(t)
 
-        assert msg_recv_list[0].nbytes == args.n_bytes
+            assert msg_recv_list[0].nbytes == args.n_bytes
+
         for i in range(args.n_iter):
-            blocking_recv(worker, ep, msg_recv_list[i])
-            blocking_send(worker, ep, msg_recv_list[i])
+            if args.enable_am is True:
+                recv = blocking_am_recv(worker, ep)
+                blocking_am_send(worker, ep, recv)
+            else:
+                blocking_recv(worker, ep, msg_recv_list[i])
+                blocking_send(worker, ep, msg_recv_list[i])
 
         finished[0] = True
 
-    listener = ucx_api.UCXListener(worker=worker, port=0, cb_func=_listener_handler)
+    listener = ucx_api.UCXListener(
+        worker=worker, port=args.port or 0, cb_func=_listener_handler
+    )
     queue.put(listener.port)
 
     while finished[0] is False:
@@ -93,14 +136,16 @@ def client(queue, port, server_address, args):
     if args.client_cpu_affinity >= 0:
         os.sched_setaffinity(0, [args.client_cpu_affinity])
 
-    if args.object_type == "numpy":
-        import numpy as np
-    elif args.object_type == "cupy":
-        import cupy as np
+    import numpy as np
 
-        np.cuda.runtime.setDevice(args.client_dev)
+    if args.object_type == "numpy":
+        import numpy as xp
+    elif args.object_type == "cupy":
+        import cupy as xp
+
+        xp.cuda.runtime.setDevice(args.client_dev)
     else:
-        import cupy as np
+        import cupy as xp
 
         import rmm
 
@@ -110,51 +155,67 @@ def client(queue, port, server_address, args):
             initial_pool_size=args.rmm_init_pool_size,
             devices=[args.client_dev],
         )
-        np.cuda.runtime.setDevice(args.client_dev)
-        np.cuda.set_allocator(rmm.rmm_cupy_allocator)
+        xp.cuda.runtime.setDevice(args.client_dev)
+        xp.cuda.set_allocator(rmm.rmm_cupy_allocator)
 
-    ctx = ucx_api.UCXContext(feature_flags=(ucx_api.Feature.TAG,))
+    ctx = ucx_api.UCXContext(
+        feature_flags=(
+            ucx_api.Feature.AM if args.enable_am is True else ucx_api.Feature.TAG,
+        )
+    )
     worker = ucx_api.UCXWorker(ctx)
+    register_am_allocators(args, worker)
     ep = ucx_api.UCXEndpoint.create(
         worker, server_address, port, endpoint_error_handling=True
     )
 
-    msg_send_list = []
-    msg_recv_list = []
-    if not args.reuse_alloc:
-        for i in range(args.n_iter):
-            msg_send_list.append(np.arange(args.n_bytes, dtype="u1"))
-            msg_recv_list.append(np.zeros(args.n_bytes, dtype="u1"))
+    if args.enable_am:
+        msg = xp.arange(args.n_bytes, dtype="u1")
     else:
-        t1 = np.arange(args.n_bytes, dtype="u1")
-        t2 = np.zeros(args.n_bytes, dtype="u1")
-        for i in range(args.n_iter):
-            msg_send_list.append(t1)
-            msg_recv_list.append(t2)
-    assert msg_send_list[0].nbytes == args.n_bytes
-    assert msg_recv_list[0].nbytes == args.n_bytes
+        msg_send_list = []
+        msg_recv_list = []
+        if not args.reuse_alloc:
+            for i in range(args.n_iter):
+                msg_send_list.append(xp.arange(args.n_bytes, dtype="u1"))
+                msg_recv_list.append(xp.zeros(args.n_bytes, dtype="u1"))
+        else:
+            t1 = xp.arange(args.n_bytes, dtype="u1")
+            t2 = xp.zeros(args.n_bytes, dtype="u1")
+            for i in range(args.n_iter):
+                msg_send_list.append(t1)
+                msg_recv_list.append(t2)
+        assert msg_send_list[0].nbytes == args.n_bytes
+        assert msg_recv_list[0].nbytes == args.n_bytes
+
     if args.cuda_profile:
-        np.cuda.profiler.start()
+        xp.cuda.profiler.start()
     times = []
     for i in range(args.n_iter):
         start = clock()
-        blocking_send(worker, ep, msg_send_list[i])
-        blocking_recv(worker, ep, msg_recv_list[i])
+        if args.enable_am:
+            blocking_am_send(worker, ep, msg)
+            blocking_am_recv(worker, ep)
+        else:
+            blocking_send(worker, ep, msg_send_list[i])
+            blocking_recv(worker, ep, msg_recv_list[i])
         stop = clock()
         times.append(stop - start)
     if args.cuda_profile:
-        np.cuda.profiler.stop()
+        xp.cuda.profiler.stop()
 
     assert len(times) == args.n_iter
     print("Roundtrip benchmark")
     print("--------------------------")
-    print(f"n_iter      | {args.n_iter}")
-    print(f"n_bytes     | {format_bytes(args.n_bytes)}")
-    print(f"object      | {args.object_type}")
-    print(f"reuse alloc | {args.reuse_alloc}")
+    print(f"n_iter          | {args.n_iter}")
+    print(f"n_bytes         | {format_bytes(args.n_bytes)}")
+    print(f"object          | {args.object_type}")
+    print(f"reuse alloc     | {args.reuse_alloc}")
+    print(f"transfer API    | {'AM' if args.enable_am else 'TAG'}")
+    print(f"UCX_TLS         | {ucp.get_config()['TLS']}")
+    print(f"UCX_NET_DEVICES | {ucp.get_config()['NET_DEVICES']}")
     print("==========================")
     if args.object_type == "numpy":
-        print("Device(s)   | CPU-only")
+        print("Device(s)       | CPU-only")
         s_aff = (
             args.server_cpu_affinity
             if args.server_cpu_affinity >= 0
@@ -165,13 +226,14 @@ def client(queue, port, server_address, args):
             if args.client_cpu_affinity >= 0
             else "affinity not set"
         )
-        print(f"Server CPU  | {s_aff}")
-        print(f"Client CPU  | {c_aff}")
+        print(f"Server CPU      | {s_aff}")
+        print(f"Client CPU      | {c_aff}")
     else:
-        print(f"Device(s)   | {args.server_dev}, {args.client_dev}")
-    print(
-        f"Average     | {format_bytes(2 * args.n_iter * args.n_bytes / sum(times))}/s"
-    )
+        print(f"Device(s)       | {args.server_dev}, {args.client_dev}")
+    avg = format_bytes(2 * args.n_iter * args.n_bytes / sum(times))
+    med = format_bytes(2 * args.n_bytes / np.median(times))
+    print(f"Average         | {avg}/s")
+    print(f"Median          | {med}/s")
     print("--------------------------")
     print("Iterations")
     print("--------------------------")
@@ -283,9 +345,22 @@ def parse_args():
         "--client-only",
         default=False,
         action="store_true",
-        help="Connect to soliatry server process (to be user with --server-only)",
+        help="Connect to solitary server process (to be user with --server-only)",
     )
-    parser.add_argument("-p", "--port", default=None, help="server port.", type=int)
+    parser.add_argument(
+        "-p",
+        "--port",
+        default=None,
+        help="The port the server will bind to, if not specified, UCX will bind "
+        "to a random port. Must be specified when --client-only is used.",
+        type=int,
+    )
+    parser.add_argument(
+        "--enable-am",
+        default=False,
+        action="store_true",
+        help="Use Active Message API instead of TAG for transfers",
+    )
 
     args = parser.parse_args()
     if args.cuda_profile and args.object_type == "numpy":
@@ -303,15 +378,19 @@ def main():
     # if you are the client, only start the `client process`
     # otherwise, start everything
 
+    if args.enable_am and not ucp._libs.ucx_api.is_am_supported():
+        print("AM only supported in UCX >= 1.11")
+        return
+
     if not args.client_only:
         # server process
         q1 = mp.Queue()
         p1 = mp.Process(target=server, args=(q1, args))
         p1.start()
         port = q1.get()
+        print(f"Server Running at {server_address}:{port}")
     else:
         port = args.port
-    print(f"Server Running at {server_address}:{port}")
 
     if not args.server_only or args.client_only:
         # client process
