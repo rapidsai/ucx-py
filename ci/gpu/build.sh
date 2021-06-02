7f2c7752f2f9714f3144fdb02438329ebfe37a36
Peter Andreas Entschev <peter@entschev.com>
2021-06-02 07:29:05 -0700

    Enable local-send-recv-core benchmark in CI

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -114,5 +114,6 @@ else
 
     gpuci_logger "Run local benchmark"
     python benchmarks/local-send-recv.py -o cupy --server-dev 0 --client-dev 0 --reuse-alloc
+    python benchmarks/local-send-recv-core.py -o cupy --server-dev 0 --client-dev 0 --reuse-alloc
     python benchmarks/cudf-merge.py --chunks-per-dev 4 --chunk-size 10000 --rmm-init-pool-size 2097152
 fi
