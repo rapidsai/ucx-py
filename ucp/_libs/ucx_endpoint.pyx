20bea703ba62ce2341ec577803f151b25737d30f
Peter Andreas Entschev <peter@entschev.com>
2021-10-28 05:18:45 -0700

    Update _inflight_msgs_to_cancel instead of overwriting

--- ucp/_libs/ucx_endpoint.pyx
+++ ucp/_libs/ucx_endpoint.pyx
@@ -66,7 +66,7 @@ cdef void _err_cb(void *arg, ucp_ep_h ep, ucs_status_t status) with gil:
     # Schedule inflight messages to be canceled after all UCP progress
     # is complete. This may happen if the user called ep.recv() but
     # the remote worker errored before sending the message.
-    ucx_worker._inflight_msgs_to_cancel = inflight_msgs
+    ucx_worker._inflight_msgs_to_cancel.update(inflight_msgs)
 
 
 cdef (ucp_err_handler_cb_t, uintptr_t) _get_error_callback(
