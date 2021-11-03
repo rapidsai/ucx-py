7989563f59f2d0c265f9513b162e47a91f26543b
Peter Andreas Entschev <peter@entschev.com>
2021-11-03 15:38:02 -0700

    Add close callback async test with AM API

--- tests/test_endpoint.py
+++ tests/test_endpoint.py
@@ -9,7 +9,8 @@ import ucp
     reason="Endpoint error handling is unreliable in UCX releases prior to 1.11.0",
 )
 @pytest.mark.parametrize("server_close_callback", [True, False])
-async def test_close_callback(server_close_callback):
+@pytest.mark.parametrize("transfer_api", ["am", "tag"])
+async def test_close_callback(server_close_callback, transfer_api):
     closed = [False]
 
     def _close_callback():
@@ -18,8 +19,11 @@ async def test_close_callback(server_close_callback):
     async def server_node(ep):
         if server_close_callback is True:
             ep.set_close_callback(_close_callback)
-        msg = bytearray(10)
-        await ep.recv(msg)
+        if transfer_api == "am":
+            await ep.am_recv()
+        else:
+            msg = bytearray(10)
+            await ep.recv(msg)
         if server_close_callback is False:
             await ep.close()
 
@@ -27,7 +31,10 @@ async def test_close_callback(server_close_callback):
         ep = await ucp.create_endpoint(ucp.get_address(), port,)
         if server_close_callback is False:
             ep.set_close_callback(_close_callback)
-        await ep.send(bytearray(b"0" * 10))
+        if transfer_api == "am":
+            await ep.am_send(bytearray(b"0" * 10))
+        else:
+            await ep.send(bytearray(b"0" * 10))
         if server_close_callback is True:
             await ep.close()
 
