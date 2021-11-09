54cfa03672a7cde8bff502d94ccd0c699915134b
Peter Andreas Entschev <peter@entschev.com>
2021-11-09 13:47:41 -0800

    Fix core probe test

--- ucp/_libs/tests/test_probe.py
+++ ucp/_libs/tests/test_probe.py
@@ -2,6 +2,7 @@ import multiprocessing as mp
 
 import pytest
 
+from ucp import get_address
 from ucp._libs import ucx_api
 from ucp._libs.utils_test import (
     blocking_am_recv,
@@ -12,12 +13,16 @@ from ucp._libs.utils_test import (
 
 mp = mp.get_context("spawn")
 
+WireupMessage = bytearray(b"wireup")
+DataMessage = bytearray(b"0" * 10)
+
 
 def _server_probe(queue, transfer_api):
-    """Server that send received message back to the client
+    """Server that probes and receives message after client disconnected.
 
-    Notice, since it is illegal to call progress() in call-back functions,
-    we use a "chain" of call-back functions.
+    Note that since it is illegal to call progress() in callback functions,
+    we keep a reference to the endpoint after the listener callback has
+    terminated, this way we can progress even after Python blocking calls.
     """
     feature_flags = (
         ucx_api.Feature.AM if transfer_api == "am" else ucx_api.Feature.TAG,
@@ -25,53 +30,67 @@ def _server_probe(queue, transfer_api):
     ctx = ucx_api.UCXContext(feature_flags=feature_flags)
     worker = ucx_api.UCXWorker(ctx)
 
-    listener_finished = [False]
+    # Keep endpoint to be used from outside the listener callback
+    global ep
+    ep = None
 
     def _listener_handler(conn_request):
         global ep
         ep = ucx_api.UCXEndpoint.create_from_conn_request(
             worker, conn_request, endpoint_error_handling=True,
         )
-        while ep.is_alive():
-            worker.progress()
-
-        if transfer_api == "am":
-            assert ep.am_probe() is True
-
-            received = blocking_am_recv(worker, ep)
-        else:
-            assert worker.tag_probe(0) is True
-
-            received = bytearray(10)
-            blocking_recv(worker, ep, received)
-
-        assert received == bytearray(b"0" * 10)
-
-        listener_finished[0] = True
 
     listener = ucx_api.UCXListener(worker=worker, port=0, cb_func=_listener_handler)
     queue.put(listener.port),
 
-    while listener_finished[0] is False:
+    while ep is None:
         worker.progress()
 
+    # Ensure wireup and inform client before it can disconnect
+    if transfer_api == "am":
+        wireup = blocking_am_recv(worker, ep)
+    else:
+        wireup = bytearray(len(WireupMessage))
+        blocking_recv(worker, ep, wireup)
+    queue.put("wireup completed")
 
-def _client_probe(port, transfer_api):
-    msg = bytearray(b"0" * 10)
+    # Ensure client has disconnected -- endpoint is not alive anymore
+    while ep.is_alive() is True:
+        worker.progress()
 
+    # Probe/receive message even after the remote endpoint has disconnected
+    if transfer_api == "am":
+        while ep.am_probe() is False:
+            worker.progress()
+        received = blocking_am_recv(worker, ep)
+    else:
+        while worker.tag_probe(0) is False:
+            worker.progress()
+        received = bytearray(len(DataMessage))
+        blocking_recv(worker, ep, received)
+
+    assert wireup == WireupMessage
+    assert received == DataMessage
+
+
+def _client_probe(queue, transfer_api):
     feature_flags = (
         ucx_api.Feature.AM if transfer_api == "am" else ucx_api.Feature.TAG,
     )
     ctx = ucx_api.UCXContext(feature_flags=feature_flags)
     worker = ucx_api.UCXWorker(ctx)
+    port = queue.get()
     ep = ucx_api.UCXEndpoint.create(
-        worker, "localhost", port, endpoint_error_handling=True,
+        worker, get_address(), port, endpoint_error_handling=True,
     )
 
-    if transfer_api == "am":
-        blocking_am_send(worker, ep, msg)
-    else:
-        blocking_send(worker, ep, msg)
+    _send = blocking_am_send if transfer_api == "am" else blocking_send
+
+    _send(worker, ep, WireupMessage)
+    _send(worker, ep, DataMessage)
+
+    # Wait for wireup before disconnecting
+    assert queue.get() == "wireup completed"
 
 
 @pytest.mark.skipif(
@@ -83,8 +102,7 @@ def test_message_probe(transfer_api):
     queue = mp.Queue()
     server = mp.Process(target=_server_probe, args=(queue, transfer_api),)
     server.start()
-    port = queue.get()
-    client = mp.Process(target=_client_probe, args=(port, transfer_api),)
+    client = mp.Process(target=_client_probe, args=(queue, transfer_api),)
     client.start()
     client.join(timeout=10)
     server.join(timeout=10)
