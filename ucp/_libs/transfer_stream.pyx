4fbad54dcaa2099ffbcd4197dc9213ea5f765225
dmitrygx <dmitrygla@mellanox.com>
2021-12-02 01:56:19 +0300

    Handle UCS_ERR_UNREACHABLE status in stream recv callback (#820)

--- ucp/_libs/transfer_stream.pyx
+++ ucp/_libs/transfer_stream.pyx
@@ -12,6 +12,7 @@ from .exceptions import (
     UCXError,
     UCXMsgTruncated,
     UCXNotConnected,
+    UCXUnreachable,
     log_errors,
 )
 from .ucx_api_dep cimport *
@@ -122,6 +123,10 @@ cdef void _stream_recv_callback(
             name = req_info["name"]
             msg = "<%s>: " % name
             exception = UCXNotConnected(msg)
+        elif status == UCS_ERR_UNREACHABLE:
+            name = req_info["name"]
+            msg = "<%s>: " % name
+            exception = UCXUnreachable(msg)
         elif status != UCS_OK:
             name = req_info["name"]
             ucx_status_msg = ucs_status_string(status).decode("utf-8")
