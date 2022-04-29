a618ffbf6a2a1113c2779a3d0d5883587c25b43a
Lawrence Mitchell <lmitchell@nvidia.com>
2022-04-29 12:17:46 +0100

    Use patch.dict to modify environment state (#844)
    To avoid changing the environment for later tests, patch the os.environ
    dict and restore it after test exit. Fixes #842.

--- tests/test_from_worker_address_error.py
+++ tests/test_from_worker_address_error.py
@@ -3,6 +3,7 @@ import multiprocessing as mp
 import os
 import re
 import time
+from unittest.mock import patch
 
 import numpy as np
 import pytest
@@ -121,12 +122,16 @@ def _test_from_worker_address_error_client(q1, q2, error_type):
         "timeout_recv",
     ],
 )
+@patch.dict(
+    os.environ,
+    {
+        "UCX_WARN_UNUSED_ENV_VARS": "n",
+        # Set low timeouts to ensure tests quickly raise as expected
+        "UCX_KEEPALIVE_INTERVAL": "100ms",
+        "UCX_UD_TIMEOUT": "100ms",
+    },
+)
 def test_from_worker_address_error(error_type):
-    os.environ["UCX_WARN_UNUSED_ENV_VARS"] = "n"
-    # Set low timeouts to ensure tests quickly raise as expected
-    os.environ["UCX_KEEPALIVE_INTERVAL"] = "100ms"
-    os.environ["UCX_UD_TIMEOUT"] = "100ms"
-
     q1 = mp.Queue()
     q2 = mp.Queue()
 
