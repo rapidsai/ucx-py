e7fa6251aeac56063ff6f7792aada5a0fc50e545
jakirkham <jakirkham@gmail.com>
2022-11-03 20:25:32 -0700

    Move most of `setup.py` into `pyproject.toml`

--- setup.py
+++ setup.py
@@ -10,13 +10,10 @@ import re
 from distutils.sysconfig import get_config_var, get_python_inc
 
 from Cython.Distutils.build_ext import new_build_ext as build_ext
-from setuptools import find_packages, setup
+from setuptools import setup
 from setuptools.extension import Extension
 
 import versioneer
-
-with open("README.md", "r") as fh:
-    readme = fh.read()
 
 include_dirs = [os.path.dirname(get_python_inc())]
 library_dirs = [get_config_var("LIBDIR")]
@@ -59,44 +56,8 @@ ext_modules = [
 cmdclass = dict(build_ext=build_ext)
 cmdclass = versioneer.get_cmdclass(cmdclass)
 
-install_requires = [
-    "numpy",
-    "psutil",
-    "pynvml",
-]
-
-extras_require = {
-    "test": [
-        "pytest",
-        "pytest-asyncio",
-    ],
-}
-
 setup(
-    name="ucx-py",
-    packages=find_packages(exclude=["tests*"]),
     ext_modules=ext_modules,
     cmdclass=cmdclass,
     version=versioneer.get_version(),
-    python_requires=">=3.6",
-    install_requires=install_requires,
-    extras_require=extras_require,
-    description="Python Bindings for the Unified Communication X library (UCX)",
-    long_description=readme,
-    author="NVIDIA Corporation",
-    license="BSD-3-Clause",
-    license_files=["LICENSE"],
-    zip_safe=False,
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: System :: Hardware",
-        "Topic :: System :: Systems Administration",
-        "Programming Language :: Python :: 3",
-    ],
-    url="https://github.com/rapidsai/ucx-py",
 )
