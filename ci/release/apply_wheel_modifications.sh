0f5d03805edcfba7ff95ededf1c222c0bc2c7370
divyegala <divyegala@gmail.com>
2023-05-04 08:23:30 -0700

    sed cupy-cuda12x in pyproject.toml

--- ci/release/apply_wheel_modifications.sh
+++ ci/release/apply_wheel_modifications.sh
@@ -8,3 +8,7 @@ CUDA_SUFFIX=${2}
 
 sed -i "s/^version = .*/version = \"${VERSION}\"/g" pyproject.toml
 sed -i "s/^name = \"ucx-py\"/name = \"ucx-py${CUDA_SUFFIX}\"/g" pyproject.toml
+
+if [[ $CUDA_SUFFIX == "-cu12" ]]; then
+    sed -i "s/cupy-cuda11x/cupy-cuda12x/g" pyproject.toml
+fi
