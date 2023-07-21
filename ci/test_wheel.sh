853ef790d7129c2797118e10e68d633e2371910c
Vyas Ramasubramani <vyasr@nvidia.com>
2023-07-21 16:17:18 -0700

    Put back underscore

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -8,7 +8,7 @@ RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 RAPIDS_PY_WHEEL_NAME="ucx_py_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./dist
 
 # echo to expand wildcard before adding `[extra]` requires for pip
-python -m pip install $(echo ./dist/ucx-py*.whl)[test]
+python -m pip install $(echo ./dist/ucx_py*.whl)[test]
 
 cd tests
 python -m pytest --cache-clear -vs .
