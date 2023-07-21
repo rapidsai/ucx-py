f926d8b5d996c510756f35a3d013983b0d31095c
Vyas Ramasubramani <vyasr@nvidia.com>
2023-07-21 10:13:36 -0700

    Switch to local scripts

---
+++ ci/test_wheel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Copyright (c) 2023, NVIDIA CORPORATION.
+
+set -eoxu pipefail
+
+mkdir -p ./dist
+RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
+RAPIDS_PY_WHEEL_NAME="cudf_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./dist
+
+# echo to expand wildcard before adding `[extra]` requires for pip
+python -m pip install $(echo ./dist/cudf*.whl)[test]
+
+cd tests
+python -m pytest --cache-clear -vs .
+cd ../ucp
+# skipped test context: https://github.com/rapidsai/ucx-py/issues/797
+python -m pytest -k 'not test_send_recv_am' --cache-clear -vs ./_libs/tests/
