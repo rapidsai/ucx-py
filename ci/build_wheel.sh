1db70a191ce5d98b0f70bca203759e93b4aba4ff
Vyas Ramasubramani <vyasr@nvidia.com>
2023-07-21 16:08:31 -0700

    Only update the RPATH for non-symlink

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -43,11 +43,13 @@ cp -P /usr/lib/ucx/* .
 # we also amend the rpath to search one directory above to *find* libuc{tsm}
 for f in libu*.so*
 do
-  patchelf --replace-needed libuct.so.0 $LIBUCT $f
-  patchelf --replace-needed libucs.so.0 $LIBUCS $f
-  patchelf --replace-needed libucm.so.0 $LIBUCM $f
-  patchelf --replace-needed libnuma.so.1 $LIBNUMA $f
-  patchelf --add-rpath '$ORIGIN/..' $f
+    if [[ ! -L $f ]]; then
+        patchelf --replace-needed libuct.so.0 $LIBUCT $f
+        patchelf --replace-needed libucs.so.0 $LIBUCS $f
+        patchelf --replace-needed libucm.so.0 $LIBUCM $f
+        patchelf --replace-needed libnuma.so.1 $LIBNUMA $f
+        patchelf --add-rpath '$ORIGIN/..' $f
+    fi
 done
 
 # Bring in cudart as well. To avoid symbol collision with other libraries e.g.
