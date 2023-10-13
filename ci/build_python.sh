6428666e79e71e685db637cd3a2e63121a796f43
Vyas Ramasubramani <vyasr@nvidia.com>
2023-10-13 14:59:40 -0700

    Update versioning strategy

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -7,9 +7,16 @@ source rapids-env-update
 
 rapids-print-env
 
+version=$(rapids-generate-version)
+commit=$(git rev-parse HEAD)
+
+version_file="ucp/_version.py"
+sed -i "/^__version__/ s/= .*/= ${version}/g" ${version_file}
+sed -i "/^__git_commit__/ s/= .*/= \"${commit}\"/g" ${version_file}
+
 rapids-logger "Begin py build"
 
-rapids-conda-retry mambabuild \
+RAPIDS_PACKAGE_VERSION=${version} rapids-conda-retry mambabuild \
   conda/recipes/ucx-py
 
 rapids-upload-conda-to-s3 python
