15b96ae0dc25bba00e4f207d959d92daab675180
ptaylor <paul.e.taylor@me.com>
2024-02-06 22:47:15 -0800

    use python -m pytest

--- ci/run_pytests.sh
+++ ci/run_pytests.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 # Test with TCP/Sockets
 # Support invoking run_pytests.sh outside the script directory
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../tests/
-timeout 10m py.test --cache-clear -vs  "$@" .
+timeout 10m python -m pytest --cache-clear -vs  "$@" .
 
 # Support invoking run_pytests.sh outside the script directory
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../ucp/
-timeout 2m py.test --cache-clear -vs  "$@" ./_libs/tests
+timeout 2m python -m pytest --cache-clear -vs  "$@" ./_libs/tests
