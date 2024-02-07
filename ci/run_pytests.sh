69a885c84b165a7ae15fbe2ca6c0afe78050746e
ptaylor <paul.e.taylor@me.com>
2024-02-06 23:13:27 -0800

    set UCX_TLS default

--- ci/run_pytests.sh
+++ ci/run_pytests.sh
@@ -6,6 +6,7 @@ set -euo pipefail
 # Support invoking run_pytests.sh outside the script directory
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../
 
-# Test with TCP/Sockets
-timeout 10m pytest --cache-clear -vs  "$@" tests
-timeout 2m pytest --cache-clear -vs  "$@" ucp/_libs/tests
+export UCX_TLS="${UCX_TLS:-tcp,cuda_copy}"
+
+timeout 10m pytest --cache-clear -vs "$@" tests
+timeout 2m pytest --cache-clear -vs "$@" ucp/_libs/tests
