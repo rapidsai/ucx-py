5fd0b04c77df8f54485bb7653a360d4af5078611
James Lamb <jlamb@nvidia.com>
2024-06-10 11:33:13 -0700

    test with https://github.com/rapidsai/rapids-build-backend/pull/41

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -11,6 +11,19 @@ source rapids-date-string
 
 rapids-print-env
 
+# TODO: remove before merging (when new rapids-build-backend is released)
+git clone \
+    -b setuptools \
+    https://github.com/jameslamb/rapids-build-backend.git \
+    /tmp/delete-me/rapids-build-backend
+
+pushd /tmp/delete-me/rapids-build-backend
+sed -e 's/^version =.*/version = "0.3.1"/' -i pyproject.toml
+python -m pip install .
+popd
+
+export PIP_FIND_LINKS="file:///tmp/delete-me/rapids-build-backend/dist"
+
 rapids-generate-version > ./VERSION
 
 rapids-logger "Begin py build"
