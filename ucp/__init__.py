ae26bb81f14b81b98efa5ab106ecf72f8483954e
Peter Andreas Entschev <peter@entschev.com>
2024-06-28 04:43:09 -0700

    Disable protov2 by default

--- ucp/__init__.py
+++ ucp/__init__.py
@@ -102,6 +102,11 @@ if "UCX_MAX_RNDV_RAILS" not in os.environ and get_ucx_version() >= (1, 12, 0):
     logger.info("Setting UCX_MAX_RNDV_RAILS=1")
     os.environ["UCX_MAX_RNDV_RAILS"] = "1"
 
+if "UCX_PROTO_ENABLE" not in os.environ:
+    # UCX protov2 still doesn't support CUDA async/managed memory
+    logger.info("Setting UCX_PROTO_ENABLE=n")
+    os.environ["UCX_PROTO_ENABLE"] = "n"
+
 
 __ucx_version__ = "%d.%d.%d" % get_ucx_version()
 
