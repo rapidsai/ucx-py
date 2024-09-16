229b6a08fcf1363b62dc360fb8f8bdff97b21f24
Peter Andreas Entschev <peter@entschev.com>
2024-09-16 16:33:11 +0200

    Update version error message (#1069)
    Update version error message to match the minimum support UCX version and provide instructions to check where UCX is being loaded from.
    
    Closes #1068 .
    
    Authors:
      - Peter Andreas Entschev (https://github.com/pentschev)
    
    Approvers:
      - Mads R. B. Kristensen (https://github.com/madsbk)
    
    URL: https://github.com/rapidsai/ucx-py/pull/1069

--- ucp/__init__.py
+++ ucp/__init__.py
@@ -108,10 +108,13 @@ if "UCX_PROTO_ENABLE" not in os.environ and get_ucx_version() >= (1, 12, 0):
     os.environ["UCX_PROTO_ENABLE"] = "n"
 
 
+__ucx_min_version__ = "1.15.0"
 __ucx_version__ = "%d.%d.%d" % get_ucx_version()
 
-if get_ucx_version() < (1, 15, 0):
+if get_ucx_version() < tuple(int(i) for i in __ucx_min_version__.split(".")):
     raise ImportError(
         f"Support for UCX {__ucx_version__} has ended. Please upgrade to "
-        "1.11.1 or newer."
+        f"{__ucx_min_version__} or newer. If you believe the wrong version "
+        "is being loaded, please check the path from where UCX is loaded "
+        "by rerunning with the environment variable `UCX_LOG_LEVEL=debug`."
     )
