e5820b2367ac19a02bfb5461ec3ba27c5259cc97
James Lamb <jlamb@nvidia.com>
2024-10-29 13:17:03 -0500

    more script changes

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -3,8 +3,6 @@
 
 set -euo pipefail
 
-underscore_package_name=$(echo "${package_name}" | tr "-" "_")
-
 source rapids-date-string
 
 rapids-generate-version > ./VERSION
@@ -29,4 +27,4 @@ python -m auditwheel repair \
     --exclude "libuct.so.0" \
     dist/*
 
-RAPIDS_PY_WHEEL_NAME="${underscore_package_name}_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 final_dist
+RAPIDS_PY_WHEEL_NAME="ucx_py_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 final_dist
