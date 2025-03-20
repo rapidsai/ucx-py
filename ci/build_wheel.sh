c130bf23bd3b7b0c8660d57866d08f766ed6c9bb
VenkateshJaya <jjayabaskar@nvidia.com>
2025-03-20 15:17:22 -0700

    added wheel build location

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 
 source rapids-date-string
 
+wheel_dir=${RAPIDS_WHEEL_BLD_OUTPUT_DIR}
+
 rapids-generate-version > ./VERSION
 
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
@@ -17,9 +19,8 @@ rapids-pip-retry wheel \
     --config-settings rapidsai.disable-cuda=false \
     .
 
-mkdir -p final_dist
 python -m auditwheel repair \
-    -w final_dist \
+    -w "${wheel_dir}" \
     --exclude "libucm.so.0" \
     --exclude "libucp.so.0" \
     --exclude "libucs.so.0" \
@@ -27,6 +28,6 @@ python -m auditwheel repair \
     --exclude "libuct.so.0" \
     dist/*
 
-./ci/validate_wheel.sh final_dist
+./ci/validate_wheel.sh "${wheel_dir}"
 
-RAPIDS_PY_WHEEL_NAME="ucx_py_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 python final_dist
+RAPIDS_PY_WHEEL_NAME="ucx_py_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 python "${wheel_dir}"
