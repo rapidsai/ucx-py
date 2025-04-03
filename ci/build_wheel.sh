a9aca2064680ee9ba68fb4d8c2f1eb25e8d2f262
VenkateshJaya <jjayabaskar@nvidia.com>
2025-04-03 16:19:05 -0700

    removed wheel_dir variable from ci scripts

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -5,8 +5,6 @@ set -euo pipefail
 
 source rapids-date-string
 
-wheel_dir=${RAPIDS_WHEEL_BLD_OUTPUT_DIR}
-
 rapids-generate-version > ./VERSION
 
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
@@ -20,7 +18,7 @@ rapids-pip-retry wheel \
     .
 
 python -m auditwheel repair \
-    -w "${wheel_dir}" \
+    -w "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}" \
     --exclude "libucm.so.0" \
     --exclude "libucp.so.0" \
     --exclude "libucs.so.0" \
@@ -28,6 +26,6 @@ python -m auditwheel repair \
     --exclude "libuct.so.0" \
     dist/*
 
-./ci/validate_wheel.sh "${wheel_dir}"
+./ci/validate_wheel.sh "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
 
-RAPIDS_PY_WHEEL_NAME="ucx_py_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 python "${wheel_dir}"
+RAPIDS_PY_WHEEL_NAME="ucx_py_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 python "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
