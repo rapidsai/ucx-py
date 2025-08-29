75c49e661947d55efda3f2bcdf6d72bb1bf01e91
James Lamb <jlamb@nvidia.com>
2025-08-29 13:48:31 -0500

    restore cudf dependency (#1164)
    Contributes to https://github.com/rapidsai/build-planning/issues/208
    
    #1162 temporarily removed the `cudf` test-time dependency here, because there weren't yet CUDA 13 `cudf` packages.
    
    Those now exist (https://github.com/rapidsai/cudf/pull/19768), so this restores that dependency.
    
    Authors:
      - James Lamb (https://github.com/jameslamb)
    
    Approvers:
      - Peter Andreas Entschev (https://github.com/pentschev)
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    URL: https://github.com/rapidsai/ucx-py/pull/1164

--- ci/run_benchmark_pytests.sh
+++ ci/run_benchmark_pytests.sh
@@ -8,12 +8,5 @@ set -euo pipefail
 pushd /
 timeout 1m python -m ucp.benchmarks.send_recv -o cupy --server-dev 0 --client-dev 0 --reuse-alloc --backend ucp-async
 timeout 1m python -m ucp.benchmarks.send_recv -o cupy --server-dev 0 --client-dev 0 --reuse-alloc --backend ucp-core
-
-# TODO: run cudf_merge benchmark unconditionally once there are CUDA 13 cudf packages
-# ref: https://github.com/rapidsai/ucx-py/pull/1162#issuecomment-3210841342
-if [[ $(python -c "import cudf") ]]; then
-    timeout 1m python -m ucp.benchmarks.cudf_merge --chunks-per-dev 4 --chunk-size 10000 --rmm-init-pool-size 2097152
-else
-    echo "'cudf' not installed, skipping cudf_merge benchmark"
-fi
+timeout 1m python -m ucp.benchmarks.cudf_merge --chunks-per-dev 4 --chunk-size 10000 --rmm-init-pool-size 2097152
 popd
